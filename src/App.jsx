38f01e6829d2dcb8e7ecc7941e23e7bd72389cdd
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-10 15:26:36 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+
+      </header>
+    </div>
+  )
+}
+
+export default App
