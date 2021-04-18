e8d80d5fb62d15ca922374b9080b6c9f9a7ee1b1
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-18 21:55:37 +0300

    chore: configure jsconfig

--- apps/app/src/App.jsx
+++ apps/app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {MainView} from '@views/MainView/MainView'
+import {MainView} from '@views/MainView/MainView.jsx'
 
 const mainStyles = {
   backgroundImage:
     'url(https://images.unsplash.com/photo-1616466446987-62a71e71b629?ixid=MnwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHx8&ixlib=rb-1.2.1&dpr=1&auto=format%2Ccompress&fit=crop&w=2999&h=594%201x,%20https://images.unsplash.com/photo-1616466446987-62a71e71b629?ixid=MnwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHx8&ixlib=rb-1.2.1&dpr=2&auto=format%2Ccompress&fit=crop&w=2999&h=594%202x)'
 }
+
 
 function App() {
   return (
