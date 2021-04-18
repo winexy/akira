1924ed4f8a4f7dfbc5657db804de03ecbdd47357
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-18 21:47:03 +0300

    feat: update ui

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -15,15 +15,27 @@ export const TodoForm = forwardRef(function ItemForm(
 ) {
   const inputRef = useRef()
   const [isFocused, setIsFocused] = useState(false)
-  const height = useRef()
+  const [isVisible, setIsVisible] = useState(false)
+  const [height, setHeight] = useState()
 
   useImperativeHandle(ref, () => ({
-    focus: () => inputRef.current.focus()
+    focus: () => inputRef.current.focus(),
+    show: () => setIsVisible(true)
   }))
 
   useEffect(() => {
-    height.current = getComputedStyle(inputRef.current).height
-  }, [])
+    if (isEmpty(inputRef.current)) {
+      return
+    }
+
+    setHeight(getComputedStyle(inputRef.current).height)
+  }, [inputRef.current])
+
+  useEffect(() => {
+    if (isVisible) {
+      inputRef.current.focus()
+    }
+  }, [isVisible])
 
   function handleSubmit(e) {
     if (isEmpty(title)) {
@@ -41,54 +53,57 @@ export const TodoForm = forwardRef(function ItemForm(
 
   return (
     <div
-      className={clsx('p-4 box-content')}
+      className={clsx('box-content px-4', {
+        'pt-4': isVisible
+      })}
       style={{
-        height: isFocused ? height.current : 'auto'
+        height: isVisible ? height : 'auto'
       }}
     >
-      <div
-        className={clsx('transition ease-in duration-75 backdrop-filter', {
-          'p-4 z-20 fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm': isFocused
-        })}
-      >
-        <form onSubmit={handleSubmit}>
-          <div className="relative flex items-center">
-            <input
-              ref={inputRef}
-              className="
-              w-full pl-4 py-3 pr-12
-              text-2xl caret-white text-white placeholder-white
-              bg-white bg-opacity-10
-              border border-gray-200 border-opacity-30
-              rounded-lg shadow appearance-none
-              transition ease-in duration-150
-              focus:outline-none focus:shadow-2xl
-            "
-              placeholder="Bread..."
-              type="text"
-              value={title}
-              onFocus={() => setIsFocused(true)}
-              onBlur={() => setIsFocused(false)}
-              onInput={e => onTitleChange(e.target.value)}
-            />
-            {!isEmpty(title) && (
-              <button
-                type="button"
+      {isVisible && (
+        <div
+          className={clsx('transition ease-in duration-75 backdrop-filter', {
+            'p-4 z-20 fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm': isVisible
+          })}
+        >
+          <form onSubmit={handleSubmit}>
+            <div className="relative flex items-center">
+              <input
+                ref={inputRef}
                 className="
+                  w-full pl-4 py-3 pr-12
+                  text-2xl caret-white text-white placeholder-white
+                  bg-white bg-opacity-10
+                  border border-gray-200 border-opacity-30
+                  rounded-lg shadow appearance-none
+                  transition ease-in duration-150
+                  focus:outline-none focus:shadow-2xl
+                "
+                placeholder="Bread..."
+                type="text"
+                value={title}
+                onBlur={() => setIsVisible(false)}
+                onInput={e => onTitleChange(e.target.value)}
+              />
+              {!isEmpty(title) && (
+                <button
+                  type="button"
+                  className="
                   absolute right-0 
                   text-white text-3xl p-4 
                   transition ease-in duration-150
                   focus:outline-none 
                   active:text-gray-500
                 "
-                onClick={onReset}
-              >
-                <XCircleIcon className="h-5 w-5" />
-              </button>
-            )}
-          </div>
-        </form>
-      </div>
+                  onClick={onReset}
+                >
+                  <XCircleIcon className="h-5 w-5" />
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   )
 })
