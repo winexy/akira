eddd41c357b2b242824a571473d653a4d75ee7a2
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-18 21:04:34 +0300

    refactor: replace dnd logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {DndProvider} from 'react-dnd'
+import {TouchBackend} from 'react-dnd-touch-backend'
+
+const dndConfig = {
+  enableMouseEvents: true,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={TouchBackend} options={dndConfig}>
+      <App />
+    </DndProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
