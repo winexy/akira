ff4f0c47279600281436fed5372c8bb36ac1e84d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-24 15:43:19 +0300

    fix: fix path to tsx

--- apps/app/vite.config.js
+++ apps/app/vite.config.js
@@ -2,14 +2,17 @@ import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
 
+const srcPath = path.resolve(__dirname, 'src')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src', 'components'),
-      '@hooks': path.resolve(__dirname, 'src', 'hooks'),
-      '@views': path.resolve(__dirname, 'src', 'views')
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(srcPath, 'components'),
+      '@hooks': path.resolve(srcPath, 'hooks'),
+      '@views': path.resolve(srcPath, 'views')
     }
   }
 })
