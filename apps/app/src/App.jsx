f783b94d8a81ba7d2ad72ba1cf0fa1f3a24a1d7f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-24 14:01:04 +0300

    feat: replace outline ring

--- apps/app/src/App.jsx
+++ apps/app/src/App.jsx
@@ -21,13 +21,14 @@ function App() {
     <React.Fragment>
       <header className="sticky top-0 z-20 px-4 py-2 flex items-center bg-black bg-opacity-60 shadow-lg">
         <h1 className="font-bold text-xl text-white font-mono">Akira</h1>
-        <button 
+        <button
           className="
             ml-auto w-8 h-8 -mr-1
             flex items-center justify-center 
             text-white rounded 
             transition ease-in duration-150
             active:bg-gray-100 active:bg-opacity-20
+            focus:outline-none focus:ring
           "
         >
           <MenuIcon className="w-6 h-6" />
