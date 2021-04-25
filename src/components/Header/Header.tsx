24546c8b398cbf97a1cf98d7016cebc6b2de05f6
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-25 22:22:23 +0300

    feat: add menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import {MenuIcon} from '@heroicons/react/solid'
-import { useAuth0 } from '@auth0/auth0-react'
+import {openMenu} from '@store/menu'
 
 export const Header: React.FC = () => {
-  const auth0 = useAuth0()
-  
   return (
     <header className="sticky top-0 z-20 px-4 py-2 flex items-center bg-black bg-opacity-60 shadow-lg">
       <h1 className="font-bold text-xl text-white font-mono">Akira</h1>
@@ -17,7 +15,7 @@ export const Header: React.FC = () => {
           active:bg-gray-100 active:bg-opacity-20
           focus:outline-none focus:ring
         "
-        onClick={() => auth0.logout()}
+        onClick={() => openMenu()}
       >
         <MenuIcon className="w-6 h-6" />
       </button>
