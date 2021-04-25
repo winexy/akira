162232d5affe909606f36377ade49bd759cdd4d2
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-25 12:55:26 +0300

    fix: dnd render issue

--- apps/app/src/components/Tasks/Tasks.tsx
+++ apps/app/src/components/Tasks/Tasks.tsx
@@ -1,35 +1,23 @@
 import React from 'react'
 import {Task} from '@/components/Task/Task'
-import {useList} from 'effector-react'
-import {
-  $tasks,
-  changeTaskPosition,
-  removeTask,
-  toggleTask
-} from '@/store/tasks'
-
-function useForceUpdate() {
-  const [, updateState] = React.useState()
-  return React.useCallback(() => {
-    updateState({} as any)
-  }, [])
-}
+import {useStore} from 'effector-react'
+import {$tasks, changeTaskPosition, removeTask, toggleTask} from '@/store/tasks'
 
 export const Tasks: React.FC = () => {
-  const tasksList = useList($tasks, (task, index) => (
-    <Task
-      key={task.id}
-      task={task}
-      index={index}
-      onCheck={toggleTask}
-      onRemove={removeTask}
-      onOrderChange={changeTaskPosition}
-    />
-  ))
+  const tasks = useStore($tasks)
 
   return (
     <ul className="space-y-1 px-4">
-      {tasksList}
+      {tasks.map((task, index) => (
+        <Task
+          key={task.id}
+          task={task}
+          index={index}
+          onCheck={toggleTask}
+          onRemove={removeTask}
+          onOrderChange={changeTaskPosition}
+        />
+      ))}
     </ul>
   )
 }
