00f5725f1bd3f257c94f93d0bdd0c4bb87c15b6e
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-25 20:25:27 +0300

    feat: add auth view

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,30 @@ import {DndProvider} from 'react-dnd'
 import {TouchBackend} from 'react-dnd-touch-backend'
 import {MainView} from '@views/MainView/MainView'
 import {Header} from './components/Header/Header'
-
-const mainStyles = {
-  backgroundImage:
-    'url(https://images.unsplash.com/photo-1616466446987-62a71e71b629?ixid=MnwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHx8&ixlib=rb-1.2.1&dpr=1&auto=format%2Ccompress&fit=crop&w=2999&h=594%201x,%20https://images.unsplash.com/photo-1616466446987-62a71e71b629?ixid=MnwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHx8&ixlib=rb-1.2.1&dpr=2&auto=format%2Ccompress&fit=crop&w=2999&h=594%202x)'
-}
-
-function useBodyBackground() {
-  useEffect(() => {
-    document.body.style.backgroundImage = mainStyles.backgroundImage
-    document.body.style.backgroundSize = 'cover'
-  }, [])
-}
+import {useAuth0} from '@auth0/auth0-react'
+import {AuthView} from './views/AuthView/AuthView'
 
 const dndConfig = {
   enableMouseEvents: true
 }
 
-
-// @ts-ignore
-console.log(import.meta.env.VITE_MSG)
-
 function App() {
-  useBodyBackground()
+  const {isAuthenticated} = useAuth0()
+
+  console.log({isAuthenticated})
 
   return (
     <DndProvider backend={TouchBackend} options={dndConfig}>
-      <React.Fragment>
-        <Header />
-        <main className="flex-1">
-          <MainView />
-        </main>
-      </React.Fragment>
+      {!isAuthenticated ? (
+        <AuthView />
+      ) : (
+        <React.Fragment>
+          <Header />
+          <main className="flex-1">
+            <MainView />
+          </main>
+        </React.Fragment>
+      )}
     </DndProvider>
   )
 }
--- src/views/MainView/MainView.tsx
+++ src/views/MainView/MainView.tsx
@@ -6,9 +6,27 @@ import {Tasks} from '@/components/Tasks/Tasks'
 import {useEffect} from 'react'
 import {addTask, loadTasksFx} from '@/store/tasks'
 
+const mainStyles = {
+  backgroundImage:
+    'url(https://images.unsplash.com/photo-1616466446987-62a71e71b629?ixid=MnwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHx8&ixlib=rb-1.2.1&dpr=1&auto=format%2Ccompress&fit=crop&w=2999&h=594%201x,%20https://images.unsplash.com/photo-1616466446987-62a71e71b629?ixid=MnwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHx8&ixlib=rb-1.2.1&dpr=2&auto=format%2Ccompress&fit=crop&w=2999&h=594%202x)'
+}
+
+function useBodyBackground() {
+  useEffect(() => {
+    document.body.style.backgroundImage = mainStyles.backgroundImage
+    document.body.style.backgroundSize = 'cover'
+
+    return () => {
+      document.body.style.backgroundImage = 'none'
+    }
+  }, [])
+}
+
 export function MainView() {
   const formRef = useRef<TaskFormRef>(null)
   const [title, setTitle] = useState('')
+
+  useBodyBackground()
 
   useEffect(() => {
     loadTasksFx()
