e82f271d6eabbb4b4d87167009ae7353f795d65a
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-27 00:10:49 +0300

    feat: improve form ui

--- apps/app/src/components/TaskForm/TaskForm.tsx
+++ apps/app/src/components/TaskForm/TaskForm.tsx
@@ -4,19 +4,17 @@ import React, {
   useState,
   useLayoutEffect,
   useImperativeHandle,
-  FormEvent,
-  MouseEventHandler,
   FormEventHandler,
   useEffect
 } from 'react'
 import isEmpty from 'lodash/fp/isEmpty'
-import {XCircleIcon} from '@heroicons/react/solid'
+import {ChevronLeftIcon, XCircleIcon, XIcon} from '@heroicons/react/solid'
 import clsx from 'clsx'
 
 type TaskFormProps = {
   title: string
   onTitleChange(title: string): void
-  onSubmit(event: FormEvent): void
+  onSubmit(): void
   onVisibilityChange(isVisible: boolean): void
 }
 
@@ -24,101 +22,134 @@ export type TaskFormRef = {
   show(): void
 }
 
-export const TaskForm = forwardRef<TaskFormRef, TaskFormProps>(function ItemForm(
-  {title, onTitleChange, onSubmit, onVisibilityChange},
-  ref
-) {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const backdropRef = useRef<HTMLDivElement>(null)
-  const [isVisible, setIsVisible] = useState(false)
+export const TaskForm = forwardRef<TaskFormRef, TaskFormProps>(
+  function ItemForm({title, onTitleChange, onSubmit, onVisibilityChange}, ref) {
+    const inputRef = useRef<HTMLInputElement>(null)
+    const backdropRef = useRef<HTMLDivElement>(null)
+    const [isVisible, setIsVisible] = useState(false)
 
-  useImperativeHandle(ref, () => ({
-    show: () => setIsVisible(true)
-  }))
+    useImperativeHandle(ref, () => ({
+      show: () => setIsVisible(true)
+    }))
 
-  useEffect(() => {
-    onVisibilityChange(isVisible)
-  }, [isVisible])
+    useEffect(() => {
+      onVisibilityChange(isVisible)
+    }, [isVisible])
 
-  useLayoutEffect(() => {
-    if (isVisible && inputRef.current) {
-      inputRef.current.focus()
-    }
+    useLayoutEffect(() => {
+      if (isVisible && inputRef.current) {
+        inputRef.current.focus()
+      }
+    }, [isVisible])
 
-  }, [isVisible])
-
-  const handleSubmit: FormEventHandler = event => {
-    if (isEmpty(title)) {
+    const handleSubmit: FormEventHandler = event => {
       event.preventDefault()
-      return
+
+      if (isEmpty(title)) {
+        return
+      }
+
+      onSubmit()
     }
 
-    onSubmit(event)
-  }
+    function onReset() {
+      onTitleChange('')
 
-  function onReset() {
-    onTitleChange('')
-
-    if (inputRef.current) {
-      inputRef.current.focus()
+      if (inputRef.current) {
+        inputRef.current.focus()
+      }
     }
-  }
 
-  const onOutsideClick: MouseEventHandler = event => {
-    if (event.target === backdropRef.current) {
-      setIsVisible(false)
-    }
-  }
-
-  return (
-    <div className={clsx('box-content px-4')}>
-      {isVisible && (
-        <div
-          ref={backdropRef}
-          className={clsx('transition ease-in duration-75 backdrop-filter', {
-            'p-4 z-20 fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm': isVisible
-          })}
-          onClick={onOutsideClick}
-        >
-          <form onSubmit={handleSubmit}>
-            <div className="relative flex items-center">
-              <input
-                ref={inputRef}
+    return (
+      <div className={clsx('box-content px-4')}>
+        {isVisible && (
+          <div
+            ref={backdropRef}
+            className={clsx(
+              'flex flex-col',
+              'transition ease-in duration-75 backdrop-filter',
+              {
+                'z-20 fixed inset-0 bg-black bg-opacity-40 backdrop-blur-sm': isVisible
+              }
+            )}
+          >
+            <div className="p-2 flex items-center justify-between">
+              <button
                 className="
-                    w-full pl-4 py-3 pr-12
-                    text-2xl caret-white text-white placeholder-white
-                    bg-white bg-opacity-40
-                    border border-gray-200
-                    rounded-lg shadow appearance-none
-                    transition ease-in duration-150
-                    focus:outline-none focus:shadow-2xl
-                  "
-                placeholder="Bread..."
-                type="text"
-                value={title}
-                onInput={e => {
-                  onTitleChange((e.target as HTMLInputElement).value)
-                }}
-              />
-              {!isEmpty(title) && (
-                <button
-                  type="button"
+                  pl-2 pr-4 py-2
+                  flex justify-center items-center
+                  text-white text-lg font-bold
+                  rounded
+                  transition ease-in duration-150
+                  active:bg-white active:bg-opacity-20
+                  focus:outline-none
+                "
+                onClick={() => setIsVisible(false)}
+              >
+                <ChevronLeftIcon className="w-8 h-8 mr-2" /> Go Back
+              </button>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="relative h-full flex items-center">
+                <input
+                  ref={inputRef}
                   className="
+                    w-full px-6 py-2 flex items-center
+                    text-2xl font-bold
+                    caret-white text-white placeholder-white
+                    bg-transparent
+                    rounded-lg appearance-none
+                    transition ease-in duration-150
+                    focus:outline-none
+                  "
+                  placeholder="Type something..."
+                  type="text"
+                  value={title}
+                  onInput={e => {
+                    onTitleChange((e.target as HTMLInputElement).value)
+                  }}
+                />
+                {!isEmpty(title) && (
+                  <button
+                    type="button"
+                    className="
                       absolute right-0 
                       text-white text-3xl p-4 
                       transition ease-in duration-150
                       focus:outline-none 
                       active:text-gray-500
                     "
-                  onClickCapture={onReset}
+                    onClickCapture={onReset}
+                  >
+                    <XCircleIcon className="h-5 w-5" />
+                  </button>
+                )}
+              </div>
+            </form>
+            {!isEmpty(title) && (
+              <div className="mt-auto p-4">
+                <button
+                  className="
+                    w-full px-4 py-2 
+                    bg-indigo-500 border border-indigo-600 
+                    text-center font-bold text-white text-lg
+                    shadow-lg rounded select-none
+                    transition ease-in duration-75
+                    active:bg-indigo-600
+                    focus:outline-none
+                  "
+                  onClick={() => {
+                    setIsVisible(false)
+                    onSubmit()
+                  }}
                 >
-                  <XCircleIcon className="h-5 w-5" />
+                  Submit
                 </button>
-              )}
-            </div>
-          </form>
-        </div>
-      )}
-    </div>
-  )
-})
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    )
+  }
+)
