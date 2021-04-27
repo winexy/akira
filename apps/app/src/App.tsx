72955bda7e7cfea6e7d273563516cfd468e20baa
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-27 22:57:45 +0300

    feat: update auth/lodaing views

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -93,11 +93,9 @@ function App() {
 
   if (isLoading) {
     return (
-      <div className="flex-1 flex-col flex justify-center items-center text-red-500">
+      <div className="flex-1 flex-col flex justify-center items-center text-white">
         {SvgLoader}
-        <h1 className="mt-8 font-mono font-bold text-4xl">
-          Akira
-        </h1>
+        <h1 className="mt-8 font-mono font-bold text-4xl">Akira</h1>
       </div>
     )
   }
--- apps/app/src/views/AuthView/AuthView.tsx
+++ apps/app/src/views/AuthView/AuthView.tsx
@@ -6,8 +6,8 @@ export const AuthView: React.FC = () => {
   const {loginWithRedirect} = useAuth0()
 
   return (
-    <div className="flex-1 flex-col flex justify-center items-center px-6">
-      <h1 className="flex items-center text-indigo-500 font-bold text-4xl mb-4 self-start">
+    <div className="flex-1 flex-col flex justify-end items-center p-6">
+      <h1 className="flex items-center text-white font-bold text-4xl mb-4 self-start">
         Welcome
       </h1>
       <button
@@ -16,12 +16,12 @@ export const AuthView: React.FC = () => {
           z-10
           w-full flex justify-center items-center
           px-8 py-3
-          font-bold text-indigo-500
+          font-bold text-white
           rounded
-          border border-indigo-500
+          border border-white
           transition ease-in duration-150
-          active:bg-indigo-500
-          active:text-white
+          active:bg-white
+          active:text-gray-700
           active:shadow-xl
           focus:outline-none
         "
