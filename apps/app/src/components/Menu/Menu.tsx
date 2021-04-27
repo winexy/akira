8761f024e1a049eb7c164e8238bd7e0a3b2b7e3d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-04-28 00:09:18 +0300

    feat: improve menu

--- apps/app/src/components/Menu/Menu.tsx
+++ apps/app/src/components/Menu/Menu.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useStore} from 'effector-react'
 import {useAuth0} from '@auth0/auth0-react'
 import clsx from 'clsx'
-import {LogoutIcon, XIcon} from '@heroicons/react/solid'
+import {ChevronLeftIcon, LogoutIcon, XIcon} from '@heroicons/react/solid'
 import {$isMenuOpened, closeMenu} from '@store/menu'
 
 export const Menu: React.FC = ({children}) => {
@@ -13,25 +13,31 @@ export const Menu: React.FC = ({children}) => {
     <>
       <nav
         className={clsx(
-          'z-50 fixed inset-0',
+          'z-10 fixed ',
+          'left-0 top-0 bottom-0',
           'flex flex-col',
-          'bg-white transform',
+          'transform',
           'transition ease-out duration-300',
           {
             '-translate-x-full': !isOpened
           }
         )}
+        style={{
+          maxWidth: '90vw'
+        }}
       >
         <div className="p-4 flex">
           <div className="flex items-center mr-3">
             <img src={user.picture} className="mr-4 w-10 h-10" />
-            <strong className="text-lg truncate">{user.name}</strong>
+            <strong className="text-lg text-gray-700 truncate">
+              {user.name}
+            </strong>
           </div>
           <button
             className="
               ml-auto w-10 h-10 
               flex justify-center items-center 
-              text-dark
+              text-gray-700
               rounded
               transition ease-in duration-150
               active:bg-gray-200
@@ -39,7 +45,7 @@ export const Menu: React.FC = ({children}) => {
             "
             onClick={() => closeMenu()}
           >
-            <XIcon className="w-8 h-8" />
+            <ChevronLeftIcon className="w-8 h-8" />
           </button>
         </div>
         <ul>
@@ -48,14 +54,13 @@ export const Menu: React.FC = ({children}) => {
         <div className="mt-auto p-4">
           <button
             className="
-              w-full p-3 
+              w-full p-3
               flex items-center 
-              font-bold text-white
-              bg-red-500
-              border border-red-700
-              shadow-md rounded
+              font-bold text-red-500
               transition ease-in duration-100
-              active:bg-red-600
+              rounded-md
+              active:text-red-600
+              active:bg-red-500 active:bg-opacity-10
               focus:outline-none
             "
             onClick={() => {
@@ -70,19 +75,21 @@ export const Menu: React.FC = ({children}) => {
       </nav>
       <div
         className={clsx(
-          'flex-1 flex relative transform transition ease-in duration-150',
-          {
-            'translate-x-10 filter blur-sm': isOpened
-          }
+          'z-50',
+          'flex-1 flex relative transform transition ease-out duration-300'
         )}
+        style={{
+          transform: isOpened ? 'translateX(85%)' : 'none'
+        }}
       >
-        {isOpened && (
-          <div className="z-20 fixed inset-0 bg-black opacity-20"></div>
-        )}
         <div
-          className={clsx('flex-1 transform transition ease-in duration-150', {
-            'scale-90': isOpened
-          })}
+          className={clsx(
+            'flex-1 transform transition ease-in duration-150 bg-gray-700',
+            'transition-transform ease-in duration-300',
+            {
+              'scale-90 rounded-3xl shadow-2xl': isOpened
+            }
+          )}
         >
           {children}
         </div>
