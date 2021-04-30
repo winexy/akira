21576d769fde4d99271ceaef6c0d1f7733b9758d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-01 00:09:07 +0300

    chore: set ecma version to 2021

--- apps/app/.eslintrc.js
+++ apps/app/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
       files: ['**/*.ts?(x)'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2021,
         sourceType: 'module',
         ecmaFeatures: {
           jsx: true
