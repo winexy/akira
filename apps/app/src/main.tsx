c5259ca71755a3f1d45918ac762a4926464385e2
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-01 00:31:08 +0300

    feat: add firebase

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase/app'
 import {Auth0Provider} from '@auth0/auth0-react'
 import {config} from '@config/app'
 import './index.css'
 import App from './App'
+
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/database'
+
+firebase.initializeApp(config.firebase)
 
 ReactDOM.render(
   <React.StrictMode>
