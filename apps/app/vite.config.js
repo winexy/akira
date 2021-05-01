a7e1cb51fe6733f806b1a763c9040ece739b7512
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-01 18:17:58 +0300

    build: use rollup copy plugin

--- apps/app/vite.config.js
+++ apps/app/vite.config.js
@@ -1,11 +1,26 @@
 import {defineConfig} from 'vite'
-// import reactRefresh from '@vitejs/plugin-react-refresh'
+import copy from 'rollup-plugin-copy'
 import alias from './config/alias'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [reactRefresh()], // Context values lost when refresh happens
   resolve: {
     alias: alias.rollup
-  }
+  },
+  plugins: [
+    {
+      ...copy({
+        targets: [
+          {
+            src: 'config/netlify/_redirects',
+            dest: 'dist'
+          }
+        ],
+        verbose: true,
+        hook: 'writeBundle'
+      })
+    }
+  ]
 })
