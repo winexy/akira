1a81a96735e013934de19b026e2b9bd297356d61
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-03 15:01:05 +0300

    chore: add hello docker

---
+++ apps/app/.github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+echo "::debug ::Debug Message"
+echo "::error ::Error Messag"
+echo "::warning ::Warning Message"
+
+echo "::add-mask::$1"
+
+echo "Hello $1"
+
+time=$(date)
+
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo "hello there"
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::helo"
