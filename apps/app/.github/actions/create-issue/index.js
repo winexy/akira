311d7c2ff2d4b03e70f7a939978275a406f3c13f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-03 14:15:47 +0300

    chore: create issue

---
+++ apps/app/.github/actions/create-issue/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+function parseAssignees(input) {
+  return input ? input.split('\n') : undefined
+}
+
+try {
+  const token = core.getInput('token')
+  const title = core.getInput('title')
+  const body = core.getInput('body')
+  const assignees = parseAssignees(core.getInput('assignees'))
+
+  const octokit = new github.Github(token)
+  const {owner, repo} = github.context
+
+  const response = octokit.issues.create({
+    owner,
+    repo,
+    title,
+    body,
+    assignees
+  })
+
+  core.setOutput('issue', JSON.stringify(response.data))
+} catch (error) {
+  core.setFailed(error.message)
+}
