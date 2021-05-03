e746912b8d38d7473070eb14c632a85ef729b37d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-03 14:21:26 +0300

    chore: recompile

--- .github/actions/create-issue/dist/index.js
+++ .github/actions/create-issue/dist/index.js
@@ -2046,27 +2046,31 @@ function parseAssignees(input) {
   return input ? input.split('\n') : undefined
 }
 
-try {
-  const token = core.getInput('token')
-  const title = core.getInput('title')
-  const body = core.getInput('body')
-  const assignees = parseAssignees(core.getInput('assignees'))
+async function run() {
+  try {
+    const token = core.getInput('token')
+    const title = core.getInput('title')
+    const body = core.getInput('body')
+    const assignees = parseAssignees(core.getInput('assignees'))
 
-  const octokit = new github.Github(token)
-  const {owner, repo} = github.context
+    const octokit = github.getOctokit(token)
+    const {owner, repo} = github.context
 
-  const response = octokit.issues.create({
-    owner,
-    repo,
-    title,
-    body,
-    assignees
-  })
+    const response = await octokit.issues.create({
+      owner,
+      repo,
+      title,
+      body,
+      assignees
+    })
 
-  core.setOutput('issue', JSON.stringify(response.data))
-} catch (error) {
-  core.setFailed(error.message)
+    core.setOutput('issue', JSON.stringify(response.data))
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
+
+run()
 
 
 /***/ }),
