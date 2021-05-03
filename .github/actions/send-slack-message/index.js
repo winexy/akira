2470f8c6b1153ec0ffd4f091b435f354f8ea217b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-03 16:03:57 +0300

    feat: add blocks

--- .github/actions/send-slack-message/index.js
+++ .github/actions/send-slack-message/index.js
@@ -1,11 +1,11 @@
-const http = require('http')
+const https = require('https')
 const core = require('@actions/core')
+const github = require('@actions/github')
 
 async function run() {
-  const message = core.getInput('message')
   const webhook = core.getInput('webhook')
 
-  const res = http.request(
+  const res = https.request(
     webhook,
     {
       method: 'POST',
@@ -13,7 +13,7 @@ async function run() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        text: message
+        blocks: getBlocks()
       })
     },
     res => {
@@ -28,6 +28,38 @@ async function run() {
     core.error(error)
     core.setFailed(error.message)
   })
+
+  function getBlocks() {
+    const message = core.getInput('message')
+    const {context} = github
+
+    return [
+      {
+        type: 'section',
+        text: md(message)
+      },
+      {
+        type: 'section',
+        fields: [
+          info('Repository', context.repo.repo),
+          info('Event', context.eventName),
+          info('SHA', context.sha),
+          info('Ref', context.ref)
+        ]
+      }
+    ]
+
+    function info(label, value) {
+      return md(`*${label}:*\n${value}`)
+    }
+
+    function md(text) {
+      return {
+        type: 'mrkdwn',
+        text
+      }
+    }
+  }
 }
 
 run()
