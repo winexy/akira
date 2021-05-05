d2979601ce1e76516354be7a1db2cfd6cad5025b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-05 13:42:26 +0300

    chore: enable fast refresh

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import {defineConfig} from 'vite'
 import copy from 'rollup-plugin-copy'
 import pkg from './package.json'
 import alias from './config/alias'
-// import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [reactRefresh()], // Context values lost when refresh happens
+  plugins: [reactRefresh()], // Context values lost when refresh happens
   resolve: {
     alias: alias.rollup
   },
