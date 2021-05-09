c6709118667420aa45dfc8cadf7d6dd2886c0a53
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-09 21:48:18 +0300

    refactor(frontend/app): replace fetch task logic

--- src/lib/akira/tasks/tasks.ts
+++ src/lib/akira/tasks/tasks.ts
@@ -1,17 +1,21 @@
 import {AxiosError, AxiosInstance} from 'axios'
+import get from 'lodash/fp/get'
 import {either} from '@/utils/either'
 import {TaskT, CreateTaskDto} from '@store/tasks'
 
 export function tasks(api: AxiosInstance) {
   return {
     all() {
-      return either<AxiosError, TaskT[]>(
-        api.get('/tasks').then(res => res.data)
-      )
+      return either<AxiosError, TaskT[]>(api.get('/tasks').then(get('data')))
     },
     createTask(data: CreateTaskDto) {
       return either<AxiosError, TaskT>(
-        api.post('/tasks', data).then(res => res.data)
+        api.post('/tasks', data).then(get('data'))
+      )
+    },
+    one(id: TaskT['id']) {
+      return either<AxiosError, TaskT>(
+        api.get(`/tasks/${id}`).then(get('data'))
       )
     }
   }
