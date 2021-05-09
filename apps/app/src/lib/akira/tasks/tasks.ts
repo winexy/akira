3e42ec114bf6d17f64dfd452ff82567f2e94db65
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-10 00:26:16 +0300

    refactor: repalce complete toggle logic

--- apps/app/src/lib/akira/tasks/tasks.ts
+++ apps/app/src/lib/akira/tasks/tasks.ts
@@ -4,18 +4,21 @@ import {either} from '@/utils/either'
 import {TaskT, CreateTaskDto} from '@store/tasks'
 
 export function tasks(api: AxiosInstance) {
+  const unwrap = get('data')
+
   return {
     all() {
-      return either<AxiosError, TaskT[]>(api.get('/tasks').then(get('data')))
+      return either<AxiosError, TaskT[]>(api.get('tasks').then(unwrap))
     },
     createTask(data: CreateTaskDto) {
-      return either<AxiosError, TaskT>(
-        api.post('/tasks', data).then(get('data'))
-      )
+      return either<AxiosError, TaskT>(api.post('tasks', data).then(unwrap))
     },
     one(id: TaskT['id']) {
+      return either<AxiosError, TaskT>(api.get(`tasks/${id}`).then(unwrap))
+    },
+    completeToggle(id: TaskT['id']) {
       return either<AxiosError, TaskT>(
-        api.get(`/tasks/${id}`).then(get('data'))
+        api.patch(`tasks/${id}/complete/toggle`).then(unwrap)
       )
     }
   }
