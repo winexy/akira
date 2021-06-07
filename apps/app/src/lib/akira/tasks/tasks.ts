3894f0421cf3d40196f93d54c0231fe219d67c4b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-07 22:19:41 +0300

    refactor: remove useless either wrapper

--- apps/app/src/lib/akira/tasks/tasks.ts
+++ apps/app/src/lib/akira/tasks/tasks.ts
@@ -1,33 +1,28 @@
-import {AxiosError, AxiosInstance} from 'axios'
+import {AxiosInstance} from 'axios'
 import get from 'lodash/fp/get'
-import {either} from '@/utils/either'
 import {TaskT, CreateTaskDto} from '@store/tasks'
 
 export function tasks(api: AxiosInstance) {
   const unwrap = get('data')
 
   return {
-    all() {
-      return either<AxiosError, TaskT[]>(api.get('tasks').then(unwrap))
+    all(): Promise<TaskT[]> {
+      return api.get('tasks').then(unwrap)
     },
-    createTask(data: CreateTaskDto) {
-      return either<AxiosError, TaskT>(api.post('tasks', data).then(unwrap))
+    createTask(data: CreateTaskDto): Promise<TaskT> {
+      return api.post('tasks', data).then(unwrap)
     },
-    one(id: TaskT['id']) {
-      return either<AxiosError, TaskT>(api.get(`tasks/${id}`).then(unwrap))
+    one(id: TaskT['id']): Promise<TaskT> {
+      return api.get(`tasks/${id}`).then(unwrap)
     },
-    toggleCompleted(id: TaskT['id']) {
-      return either<AxiosError, TaskT>(
-        api.patch(`tasks/${id}/complete/toggle`).then(unwrap)
-      )
+    toggleCompleted(id: TaskT['id']): Promise<TaskT> {
+      return api.patch(`tasks/${id}/complete/toggle`).then(unwrap)
     },
-    toggleImportant(id: TaskT['id']) {
-      return either<AxiosError, TaskT>(
-        api.patch(`tasks/${id}/important/toggle`).then(unwrap)
-      )
+    toggleImportant(id: TaskT['id']): Promise<TaskT> {
+      return api.patch(`tasks/${id}/important/toggle`).then(unwrap)
     },
-    delete(id: TaskT['id']) {
-      return either<AxiosError, true>(api.delete(`tasks/${id}`).then(unwrap))
+    delete(id: TaskT['id']): Promise<true> {
+      return api.delete(`tasks/${id}`).then(unwrap)
     }
   }
 }
