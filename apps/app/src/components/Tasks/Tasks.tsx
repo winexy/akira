a713bc86ec3aef39512033153db2c12d2f7d7b4f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-08 23:16:59 +0300

    feat: add skeleton loader

--- apps/app/src/components/Tasks/Tasks.tsx
+++ apps/app/src/components/Tasks/Tasks.tsx
@@ -5,13 +5,49 @@ import {
   removeTaskFx,
   toggleTaskFx,
   toggleImportantFx,
-  $tasksIds
+  $tasksIds,
+  loadTasksFx
 } from '@/store/tasks'
 import {useStore} from 'effector-react'
-import {} from '../../store/tasks/slice'
+import ContentLoader from 'react-content-loader'
+import {times} from 'lodash/fp'
 
 export const Tasks: React.FC = () => {
   const tasksIds = useStore($tasksIds)
+  const isPending = useStore(loadTasksFx.pending)
+
+  if (isPending) {
+    const taskHeight = 64
+    const spacing = 4
+    const count = 3
+    const height = taskHeight * count + (count - 1) * spacing
+    return (
+      <section className="px-4">
+        <ContentLoader
+          speed={2}
+          width="100%"
+          height={height}
+          backgroundColor="#ffffff"
+          foregroundColor="#e9e9e9"
+        >
+          {times(
+            n => (
+              <rect
+                key={n}
+                x="0"
+                y={n * taskHeight + n * spacing}
+                rx="6"
+                ry="6"
+                width="100%"
+                height={taskHeight}
+              />
+            ),
+            count
+          )}
+        </ContentLoader>
+      </section>
+    )
+  }
 
   return (
     <ul className="space-y-1 px-4">
