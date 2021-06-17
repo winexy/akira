05794d4039a9c1ac2fc0f88c921216b4783d06ac
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-17 19:59:03 +0300

    feat: add WIP page

---
+++ apps/app/src/views/WipView/WipView.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {ShieldExclamationIcon} from '@heroicons/react/solid'
+import {View} from '@views/View/View'
+import {useHistory} from 'react-router'
+
+export const WipView: React.FC = () => {
+  const history = useHistory()
+
+  return (
+    <View className="h-full flex flex-col items-center justify-center">
+      <ShieldExclamationIcon className="w-16 h-16 text-yellow-500" />
+      <h2 className="mt-4 font-bold text-3xl text-gray-700">
+        Work In Progress
+      </h2>
+      <button
+        className="
+          mt-8 px-6 py-2 
+          border border-gray-700 
+          text-gray-700 font-bold 
+          rounded shadow-sm 
+          transition ease-in duration-150
+          active:text-white active:bg-gray-600 
+          focus:outline-none
+        "
+        onClick={history.goBack}
+      >
+        Go back
+      </button>
+    </View>
+  )
+}
