d763aaee98df0c944c8e85f2557ae3f96ed6eb07
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-20 22:53:02 +0300

    feat: add mock filters

--- src/views/TodayView/index.tsx
+++ src/views/TodayView/index.tsx
@@ -16,6 +16,11 @@ import clsx from 'clsx'
 import {useStore} from 'effector-react'
 import format from 'date-fns/format'
 import {MainView} from '@views/MainView'
+import {showBottomSheet} from '@store/bottom-sheet/index'
+import {BottomSheet} from '@components/BottomSheet/BottomSheet'
+import {Checkbox} from '@components/Checkbox/Checkbox'
+import {WIP} from '@components/Tag/Tag'
+import {FilterIcon} from '@heroicons/react/outline'
 
 export function TodayView() {
   const formRef = useRef<TaskFormRef>(null)
@@ -25,6 +30,7 @@ export function TodayView() {
   const completedTasksCount = useStore($completedTasksCount)
   const isPending = useStore(queryTasksFx.pending)
   const tasksIds = useStore($tasksIds)
+  const [filters, setFilters] = useState<string[]>([])
 
   const today = format(new Date(), 'eeee, do MMMM')
 
@@ -62,11 +68,62 @@ export function TodayView() {
         <h2 className="font-bold text-3xl">Today</h2>
         <p className="text-sm">{today}</p>
       </div>
-      <div className="mt-4 px-4">
+      <div className="flex justify-between items-center mt-4 px-4">
         <span className="text-gray-700 font-bold mr-4">
           {completedTasksCount} / {size(tasksIds)} completed
         </span>
+        <button
+          className="
+            flex items-center pl-2 pr-1 py-1 
+            border border-gray-300
+            font-semibold 
+            rounded-md
+            active:bg-gray-200
+            focus:outline-none 
+          "
+          onClick={() => showBottomSheet('filters')}
+        >
+          <FilterIcon className="w-4 h-4 mr-2" />
+          Filters
+          <WIP className="ml-2" />
+        </button>
       </div>
+      <BottomSheet name="filters" className="px-6 pb-6 pt-1 text-gray-800">
+        <h2 className="font-bold text-2xl">Filters</h2>
+        <ul className="mt-4 space-y-3">
+          {['Completed', 'Important'].map(value => (
+            <li className="" key={value}>
+              <label className="flex items-center font-semibold text-lg active:text-gray-500">
+                <Checkbox
+                  className="mr-3"
+                  isChecked={filters.includes(value)}
+                  onChange={checked => {
+                    if (!checked) {
+                      setFilters(filters.filter(v => v !== value))
+                    } else {
+                      setFilters([...filters, value])
+                    }
+                  }}
+                />
+                {value}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <button
+          className="
+            mt-6 px-6 w-full py-3 
+            text-white bg-indigo-500 
+            border-indigo-600 border
+            rounded-md
+            transition ease-in duration-150
+            focus:outline-none
+            active:bg-indigo-600
+          "
+        >
+          Apply
+        </button>
+      </BottomSheet>
       <section className="mt-4">
         <Tasks isPending={isPending} tasksIds={tasksIds} />
       </section>
