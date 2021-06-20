c7d245d82d27419843624bcfeb7f1af5de558917
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-20 19:37:35 +0300

    feat: add all tasks view

---
+++ apps/app/src/views/TasksView/index.tsx
@@ -0,0 +1,25 @@
+import React, {useEffect} from 'react'
+import {ViewTemplate} from '@/views/ViewTemplate'
+import {useStore} from 'effector-react'
+import {$tasksIds, queryTasksFx} from '@store/tasks/slice'
+import {Tasks} from '@components/Tasks'
+
+export const TasksView: React.FC = () => {
+  const isPending = useStore(queryTasksFx.pending)
+  const tasksIds = useStore($tasksIds)
+
+  useEffect(() => {
+    queryTasksFx()
+  }, [])
+
+  return (
+    <ViewTemplate>
+      <div className="px-4 text-gray-600">
+        <h2 className="flex items-center font-bold text-3xl">Tasks</h2>
+      </div>
+      <section className="mt-4">
+        <Tasks isPending={isPending} tasksIds={tasksIds} />
+      </section>
+    </ViewTemplate>
+  )
+}
