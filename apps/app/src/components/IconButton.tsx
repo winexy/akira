2fcbb6ec5e9c4bf194a48f807833ec92e33bb08d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-26 16:51:01 +0300

    feat: add IconButton

---
+++ apps/app/src/components/IconButton.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx'
+import React from 'react'
+import {exhaustiveCheck} from '../utils/index'
+
+type ButtonSize = 'sm'
+
+type Props = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {size?: ButtonSize}
+
+function matchSize(size: ButtonSize) {
+  switch (size) {
+    case 'sm':
+      return 'w-8 h-8'
+    default:
+      return exhaustiveCheck(size)
+  }
+}
+
+export const IconButton: React.FC<Props> = ({size = 'sm', ...props}) => (
+  <button
+    className={clsx(
+      'flex justify-center items-center',
+      'rounded',
+      'transition ease-in duration-150',
+      'active:bg-gray-200',
+      'focus:outline-none focus-within:ring',
+      matchSize(size)
+    )}
+    type="button"
+    {...props}
+  />
+)
