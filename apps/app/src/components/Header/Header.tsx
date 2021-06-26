7a24e81acab790fea1a876bc9951bc32c87b658c
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-26 16:01:08 +0300

    feat: add partial sticky header

--- apps/app/src/components/Header/Header.tsx
+++ apps/app/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import clsx from 'clsx'
 import {MenuIcon} from '@heroicons/react/solid'
 import {LightningBoltIcon} from '@heroicons/react/outline'
@@ -7,13 +7,42 @@ import {toggleMenu} from '@store/menu'
 import {Link} from 'react-router-dom'
 
 export const Header: React.FC = ({children}) => {
+  const headerRef = useRef<HTMLElement | null>(null)
+  const [isFloating, setIsFloating] = useState(false)
+
+  useEffect(() => {
+    const root = document.getElementById('root')
+    const headerRect = headerRef.current?.getBoundingClientRect()
+
+    const listener = () => {
+      if (root && headerRect) {
+        setIsFloating(root.scrollTop > headerRect.height)
+      }
+    }
+
+    if (root) {
+      root.addEventListener('scroll', listener, false)
+    }
+
+    return () => {
+      if (root) {
+        root.removeEventListener('scroll', listener)
+      }
+    }
+  }, [])
+
   return (
     <header
+      ref={headerRef}
       className={clsx(
-        'sticky top-0 z-20 px-4 py-2',
+        'sticky w-full top-0 z-20 px-4 py-2',
         'flex items-center',
+        'transition ease-in duration-100',
         'bg-gradient-to-b from-white to-gray-100',
-        'text-gray-700'
+        'text-gray-700',
+        {
+          'shadow-2xl border-b': isFloating
+        }
       )}
     >
       {isUndefined(children) ? (
