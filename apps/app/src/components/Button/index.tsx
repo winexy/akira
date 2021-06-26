1401324bdae850c941666ffc3f76bdca9fb51751
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-26 21:25:53 +0300

    feat: add button focus states

--- apps/app/src/components/Button/index.tsx
+++ apps/app/src/components/Button/index.tsx
@@ -13,11 +13,11 @@ type Props = React.DetailedHTMLProps<
 const sm = 'px-2 py-1'
 const md = 'px-6 py-3'
 const blue =
-  'bg-blue-500 border border-blue-600 active:bg-blue-600 active:border-blue-700 text-white shadow-md active:shadow'
+  'bg-blue-500 border border-blue-600 active:bg-blue-600 active:border-blue-700 text-white shadow-md active:shadow focus-withing:ring'
 const red =
-  'bg-red-500 border border-red-600 active:bg-red-600 active:border-red-700 text-white shadow-md active:shadow'
+  'bg-red-500 border border-red-600 active:bg-red-600 active:border-red-700 text-white shadow-md active:shadow focus-within:ring-red-300'
 const outline =
-  'bg-transparent border border-gray-100 text-gray-700 shadow-none active:bg-gray-100 active:border-gray-200 active:shadow-inner'
+  'bg-transparent border border-gray-200 text-gray-700 shadow-none active:bg-gray-100 active:border-gray-200 active:shadow-inner focus-withing:ring'
 
 type ButtonSize = 'sm' | 'md'
 type ButtonVariant = 'blue' | 'red' | 'outline'
@@ -64,6 +64,7 @@ export const Button: React.FC<Props> = ({
     disabled:text-gray-300
     disabled:shadow-sm
     focus:outline-none
+    focus-within:ring
   `,
     matchSize(size),
     matchVariant(variant),
