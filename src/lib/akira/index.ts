6ed7f8ab633980c677599eec8f5e95cbc4ebb522
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-27 17:02:03 +0300

    feat: add myday api

--- src/lib/akira/index.ts
+++ src/lib/akira/index.ts
@@ -2,10 +2,12 @@ import {api} from '@lib/api'
 import {tasks} from './tasks/tasks'
 import {checklist} from './checklist/checklist'
 import {tags} from './tags'
+import {myday} from './myday'
 
 export const akira = {
   tasks: tasks(api),
   tags: tags(api),
+  myday: myday(api),
   checklist: checklist(api)
 }
 
