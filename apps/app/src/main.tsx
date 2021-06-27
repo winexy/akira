1838d7e0855193b250d0bfdad630e6e079d97835
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-27 17:00:56 +0300

    feat: prefetch today tasks

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {QueryClient, QueryClientProvider} from 'react-query'
 import './index.css'
-import {FirebaseAuthProvider} from '@/firebase/index'
+import {akira} from '@lib/akira'
+import {TaskT} from '@store/tasks/types'
+import {FirebaseAuthProvider} from '@/firebase'
 import App from './App'
+import {onMyDayFetch} from './modules/tasks/store'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false
+    }
+  }
+})
+
+async function prefetchMyDay() {
+  await queryClient.prefetchQuery('tasks:today', akira.myday.tasks)
+
+  const tasks = queryClient.getQueryData<TaskT[]>('tasks:today')
+
+  if (tasks) {
+    onMyDayFetch(tasks)
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseAuthProvider>
-      <App />
+    <FirebaseAuthProvider onAuthSuccess={prefetchMyDay}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </FirebaseAuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
