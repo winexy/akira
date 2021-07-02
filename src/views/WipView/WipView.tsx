03a7a7e894ec1727a44da970a252c5ad2bf70c24
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-02 20:17:24 +0300

    refactor: replace go back button

--- src/views/WipView/WipView.tsx
+++ src/views/WipView/WipView.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {ShieldExclamationIcon} from '@heroicons/react/solid'
 import {MainView} from '@views/MainView'
 import {useHistory} from 'react-router'
+import {Button} from '@components/Button'
 
 export const WipView: React.FC = () => {
   const history = useHistory()
@@ -12,20 +13,14 @@ export const WipView: React.FC = () => {
       <h2 className="mt-4 font-bold text-3xl text-gray-700">
         Work In Progress
       </h2>
-      <button
-        className="
-          mt-8 px-6 py-2 
-          border border-gray-700 
-          text-gray-700 font-bold 
-          rounded shadow-sm 
-          transition ease-in duration-150
-          active:text-white active:bg-gray-600 
-          focus:outline-none
-        "
+      <Button
+        size="md"
+        variant="outline"
+        className="mt-8"
         onClick={history.goBack}
       >
         Go back
-      </button>
+      </Button>
     </MainView>
   )
 }
