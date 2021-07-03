b596784d8e02ba50570fb669d7c6f54cf3bef953
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-03 13:28:02 +0300

    feat: add new list view

--- src/lib/akira/index.ts
+++ src/lib/akira/index.ts
@@ -3,11 +3,13 @@ import {tasks} from './tasks/tasks'
 import {checklist} from './checklist/checklist'
 import {tags} from './tags'
 import {myday} from './myday'
+import {list} from './list'
 
 export const akira = {
   tasks: tasks(api),
   tags: tags(api),
   myday: myday(api),
+  list: list(api),
   checklist: checklist(api)
 }
 
---
+++ src/views/NewListView/index.tsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useLayoutEffect, useRef, useState} from 'react'
+import {useMutation} from 'react-query'
+import {akira} from '@lib/akira'
+import {MainView} from '../MainView'
+
+export const NewListView: React.FC = () => {
+  const [title, setTitle] = useState('Untitled list')
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const createListMutation = useMutation(akira.list.create)
+  const {data: list} = createListMutation
+
+  useLayoutEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  useEffect(() => {
+    if (list && list.title !== title) {
+      setTitle(list.title)
+    }
+  }, [list, title])
+
+  async function onInputBlur() {
+    if (createListMutation.isIdle) {
+      createListMutation.mutateAsync(title)
+    }
+  }
+
+  return (
+    <MainView>
+      <div className="">
+        <input
+          ref={inputRef}
+          type="text"
+          value={title}
+          placeholder="Untitled list"
+          className="
+            w-full px-4 py-2
+            text-2xl font-semibold 
+            text-gray-700
+            appearance-none 
+            transition ease-in duration-150
+            focus:bg-gray-100 focus:outline-none
+          "
+          onChange={event => {
+            setTitle(event.target.value)
+          }}
+          onBlur={onInputBlur}
+        />
+      </div>
+    </MainView>
+  )
+}
