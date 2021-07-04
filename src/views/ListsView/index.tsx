61e2399a314c79d664d49d5704c31904b3018314
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-04 13:07:19 +0300

    refactor: update TaskLists

--- src/views/ListsView/index.tsx
+++ src/views/ListsView/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {useListsQuery} from '@modules/lists/hooks'
 import {TaskLists} from '@modules/lists/components/TaskLists'
 import {MainView} from '../MainView'
 
 export const ListsView: React.FC = () => {
-  const {data: lists} = useListsQuery()
-
-  return <MainView>{lists && <TaskLists lists={lists} />}</MainView>
+  return (
+    <MainView>
+      <TaskLists />
+    </MainView>
+  )
 }
