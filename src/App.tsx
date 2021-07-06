6553ad8777bebc3593bea58688555006db4fe03f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-06 22:11:17 +0300

    refactor: extract AkiraLoader

--- src/App.tsx
+++ src/App.tsx
@@ -13,85 +13,11 @@ import {TasksView} from './views/TasksView'
 import {TagsView} from './modules/tags/views'
 import {NewListView} from './views/NewListView'
 import {ListsView} from './views/ListsView'
+import {AkiraLoader} from './components/AkiraLoader'
 
 const dndConfig = {
   enableMouseEvents: true
 }
-
-const SvgLoader = (
-  <svg
-    width="57"
-    height="57"
-    viewBox="0 0 57 57"
-    xmlns="http://www.w3.org/2000/svg"
-    stroke="currentColor"
-  >
-    <g fill="none" fillRule="evenodd">
-      <g transform="translate(1 1)" strokeWidth="2">
-        <circle cx="5" cy="50" r="5">
-          <animate
-            attributeName="cy"
-            begin="0s"
-            dur="2.2s"
-            values="50;5;50;50"
-            calcMode="linear"
-            repeatCount="indefinite"
-          />
-          <animate
-            attributeName="cx"
-            begin="0s"
-            dur="2.2s"
-            values="5;27;49;5"
-            calcMode="linear"
-            repeatCount="indefinite"
-          />
-        </circle>
-        <circle cx="27" cy="5" r="5">
-          <animate
-            attributeName="cy"
-            begin="0s"
-            dur="2.2s"
-            from="5"
-            to="5"
-            values="5;50;50;5"
-            calcMode="linear"
-            repeatCount="indefinite"
-          />
-          <animate
-            attributeName="cx"
-            begin="0s"
-            dur="2.2s"
-            from="27"
-            to="27"
-            values="27;49;5;27"
-            calcMode="linear"
-            repeatCount="indefinite"
-          />
-        </circle>
-        <circle cx="49" cy="50" r="5">
-          <animate
-            attributeName="cy"
-            begin="0s"
-            dur="2.2s"
-            values="50;50;5;50"
-            calcMode="linear"
-            repeatCount="indefinite"
-          />
-          <animate
-            attributeName="cx"
-            from="49"
-            to="49"
-            begin="0s"
-            dur="2.2s"
-            values="49;5;27;49"
-            calcMode="linear"
-            repeatCount="indefinite"
-          />
-        </circle>
-      </g>
-    </g>
-  </svg>
-)
 
 function App() {
   const {isLoading, isAuthenticated} = useFirebaseAuth()
@@ -99,7 +25,7 @@ function App() {
   if (isLoading) {
     return (
       <div className="flex-1 flex-col flex justify-center items-center text-white">
-        {SvgLoader}
+        <AkiraLoader />
         <h1 className="mt-8 font-mono font-bold text-4xl">Akira</h1>
       </div>
     )
