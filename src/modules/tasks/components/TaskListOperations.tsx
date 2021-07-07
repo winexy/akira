575adfecb486355bd6e7d5742bef7e18325fa6f4
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-07 23:31:50 +0300

    feat: decompose task list operations

---
+++ src/modules/tasks/components/TaskListOperations.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import clsx from 'clsx'
+import {FilterIcon, SortAscendingIcon} from '@heroicons/react/solid'
+import {showBottomSheet} from '@store/bottom-sheet'
+
+type Props = {
+  isFiltered: boolean
+  isSorted: boolean
+}
+
+export const TaskListOperations: React.FC<Props> = ({isFiltered, isSorted}) => {
+  return (
+    <div className="fixed bottom-0 right-0 left-0 py-7 flex items-center mt-4 px-4 from-gray-100 bg-gradient-to-t">
+      <button
+        className={clsx(
+          'flex items-center px-3 py-1 ',
+          'border',
+          'font-semibold ',
+          'rounded-md shadow-md',
+          'active:shadow-lg',
+          'focus:outline-none ',
+          isFiltered
+            ? 'text-white bg-blue-500 border-blue-600 active:bg-blue-600 active:border-blue-700'
+            : 'bg-gray-100 border-gray-300 active:bg-gray-200'
+        )}
+        onClick={() => showBottomSheet('filters')}
+      >
+        <FilterIcon className="w-4 h-4 mr-2" />
+        Filters
+      </button>
+      <button
+        className={clsx(
+          'ml-4 flex items-center px-3 py-1 ',
+          'border',
+          'font-semibold',
+          'rounded-md shadow-md',
+          'active:bg-gray-200',
+          'active:shadow-lg',
+          'focus:outline-none',
+          isSorted
+            ? 'text-white bg-blue-500 border-blue-600 active:bg-blue-600 active:border-blue-700'
+            : 'bg-gray-100 border-gray-300 active:bg-gray-200'
+        )}
+        onClick={() => showBottomSheet('sorting')}
+      >
+        <SortAscendingIcon className="w-4 h-4 mr-2" />
+        Sort
+      </button>
+    </div>
+  )
+}
