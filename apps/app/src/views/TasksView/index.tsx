ba7e4433f4c4e3bb3fc251f8d2eace010984337b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-07 23:31:50 +0300

    feat: decompose task list operations

--- apps/app/src/views/TasksView/index.tsx
+++ apps/app/src/views/TasksView/index.tsx
@@ -1,22 +1,36 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {MainView} from '@/views/MainView'
 import {Tasks} from '@components/Tasks'
 import {useTasksQuery} from '@modules/tasks/hooks'
 import {TaskForm} from '@components/TaskForm/TaskForm'
 import {akira} from '@lib/akira'
 import {useMutation} from 'react-query'
+import {FiltersBottomSheet} from '@modules/tasks/filters/FiltersBottomSheet'
+import {
+  SortingBottomSheet,
+  useTaskSorting
+} from '@modules/tasks/sorting/SortingBottomSheet'
 import {
   AddTaskButton,
   useAddTaskControl
 } from '@modules/tasks/components/AddTaskButton'
+import {useTaskFilters, filterTasks} from '@modules/tasks/filters'
+import size from 'lodash/fp/size'
+import {useTagsQuery} from '@modules/tags/hooks'
+import {TaskListOperations} from '@modules/tasks/components/TaskListOperations'
 
 export const TasksView: React.FC = () => {
   const {data: tasks = [], isLoading} = useTasksQuery()
+  const {data: tags = []} = useTagsQuery()
   const addTaskControl = useAddTaskControl()
+  const [filtersState, updateFilters] = useTaskFilters()
+  const {sortType, setSortType, sort} = useTaskSorting()
 
   const createTaskMutation = useMutation((title: string) => {
     return akira.tasks.create(title)
   })
+
+  const sorted = sort(filterTasks(tasks, filtersState))
 
   return (
     <MainView>
@@ -29,8 +43,19 @@ export const TasksView: React.FC = () => {
         <h2 className="flex items-center font-bold text-3xl">Tasks</h2>
       </div>
       <section className="mt-4 pb-4">
-        <Tasks isPending={isLoading} tasks={tasks} />
+        <Tasks isPending={isLoading} tasks={sorted} />
       </section>
+      <FiltersBottomSheet
+        canReset={size(tasks) !== size(sorted)}
+        state={filtersState}
+        tags={tags}
+        onChange={updateFilters}
+      />
+      <SortingBottomSheet sortType={sortType} onChange={setSortType} />
+      <TaskListOperations
+        isFiltered={size(sorted) !== size(tasks)}
+        isSorted={Boolean(sortType)}
+      />
       {addTaskControl.isVisible && (
         <div className="z-20 fixed bottom-0 right-0 p-4">
           <AddTaskButton onClick={addTaskControl.onAddIntent} />
--- apps/app/src/views/TodayView/index.tsx
+++ apps/app/src/views/TodayView/index.tsx
@@ -5,11 +5,8 @@ import {Tasks} from '@components/Tasks'
 import size from 'lodash/fp/size'
 import filter from 'lodash/fp/filter'
 
-import clsx from 'clsx'
 import format from 'date-fns/format'
 import {MainView} from '@views/MainView'
-import {showBottomSheet} from '@store/bottom-sheet/index'
-import {FilterIcon} from '@heroicons/react/outline'
 import {useQuery, useQueryClient, useMutation} from 'react-query'
 import {akira} from '@lib/akira'
 import {onMyDayFetch} from '@modules/tasks/store'
@@ -18,6 +15,7 @@ import {TaskQueryKeyEnum} from '@modules/tasks/config/index'
 import {useTagsQuery} from '@modules/tags/hooks'
 import {filterTasks, useTaskFilters} from '@modules/tasks/filters'
 import {FiltersBottomSheet} from '@modules/tasks/filters/FiltersBottomSheet'
+import {TaskListOperations} from '@modules/tasks/components/TaskListOperations'
 import {
   SortingBottomSheet,
   useTaskSorting
@@ -89,44 +87,10 @@ export function TodayView() {
       <section className="mt-4 pb-24">
         <Tasks isPending={isLoading} tasks={sorted} />
       </section>
-      <div className="fixed bottom-0 right-0 left-0 py-7 flex items-center mt-4 px-4 from-gray-100 bg-gradient-to-t">
-        <button
-          className={clsx(
-            'flex items-center px-3 py-1 ',
-            'border',
-            'font-semibold ',
-            'rounded-md shadow-md',
-            'active:shadow-lg',
-            'focus:outline-none ',
-            size(sorted) !== size(tasks)
-              ? 'text-white bg-blue-500 border-blue-600 active:bg-blue-600 active:border-blue-700'
-              : 'bg-gray-100 border-gray-300 active:bg-gray-200'
-          )}
-          onClick={() => showBottomSheet('filters')}
-        >
-          <FilterIcon className="w-4 h-4 mr-2" />
-          Filters
-        </button>
-        <button
-          className={clsx(
-            'ml-4 flex items-center px-3 py-1 ',
-            'border',
-            'font-semibold',
-            'rounded-md shadow-md',
-            'active:bg-gray-200',
-            'active:shadow-lg',
-            'focus:outline-none',
-            sortType
-              ? 'text-white bg-blue-500 border-blue-600 active:bg-blue-600 active:border-blue-700'
-              : 'bg-gray-100 border-gray-300 active:bg-gray-200'
-          )}
-          onClick={() => showBottomSheet('sorting')}
-        >
-          <SortAscendingIcon className="w-4 h-4 mr-2" />
-          Sort
-          {/* <WIP className="ml-2" /> */}
-        </button>
-      </div>
+      <TaskListOperations
+        isFiltered={size(sorted) !== size(tasks)}
+        isSorted={Boolean(sortType)}
+      />
       {addTaskControl.isVisible && (
         <div className="z-20 fixed bottom-0 right-0 p-4">
           <AddTaskButton onClick={addTaskControl.onAddIntent} />
