addd00bf4a1027af1cfc6378b269c90de8e08cf8
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-11 00:09:28 +0300

    feat: update lists view

--- apps/app/src/views/ListsView/index.tsx
+++ apps/app/src/views/ListsView/index.tsx
@@ -5,7 +5,10 @@ import {MainView} from '../MainView'
 export const ListsView: React.FC = () => {
   return (
     <MainView>
-      <TaskLists />
+      <div className="px-4 flex items-center">
+        <h1 className="flex items-center font-semibold text-3xl ">Lists</h1>
+      </div>
+      <TaskLists className="mt-2 border-t border-gray-100" />
     </MainView>
   )
 }
