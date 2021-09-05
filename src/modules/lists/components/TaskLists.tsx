cee3fa0459a1606ab75d131f9d26053403cdc1da
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-09-05 23:31:41 +0300

    feat: add removing spinner

--- src/modules/lists/components/TaskLists.tsx
+++ src/modules/lists/components/TaskLists.tsx
@@ -3,13 +3,15 @@ import {Swipeable} from '@components/Swipeable/Swipeable'
 import {TrashIcon} from '@heroicons/react/solid'
 import {Link} from 'react-router-dom'
 import isUndefined from 'lodash/fp/isUndefined'
-import {useMutation, useQueryClient} from 'react-query'
+import {useIsMutating, useMutation, useQueryClient} from 'react-query'
 import clsx from 'clsx'
 import {akira} from '@lib/akira'
 import {closeMenu} from '@store/menu'
 import {ActionSheet} from '@components/ActionSheet/ActionSheet'
-import {openActionSheet} from '@store/action-sheet'
+import {openActionSheet, closeActionSheet} from '@store/action-sheet'
+import {Spin} from '@components/Spin'
 import {useListsQuery} from '../hooks/index'
+import {TaskList} from '../types.d'
 
 type Props = {
   className?: string
@@ -21,44 +23,50 @@ function pluralize(count: number, one: string, other: string) {
   return rule === 'one' ? one : other
 }
 
-export const TaskLists: React.FC<Props> = ({
-  allowRemoval = true,
-  className
-}) => {
+type TaskListItemProps = {
+  list: TaskList
+  allowRemoval: boolean
+}
+
+const TaskListItem: React.FC<TaskListItemProps> = ({list, allowRemoval}) => {
   const queryClient = useQueryClient()
-  const {data: lists} = useListsQuery()
   const removeListMutation = useMutation(akira.lists.remove, {
+    mutationKey: 'remove-tasks-list',
     onSuccess() {
       queryClient.invalidateQueries('lists')
     }
   })
 
-  if (isUndefined(lists)) {
-    return null
+  const isRemoving = Boolean(
+    useIsMutating({
+      mutationKey: 'remove-tasks-list',
+      predicate(mutation) {
+        return mutation.options.variables === list.id
+      }
+    })
+  )
+
+  function onRemoveIntent() {
+    openActionSheet(`remove-task-list(${list.id})`)
   }
 
-  function onRemoveIntent(listId: number) {
-    openActionSheet(`remove-task-list(${listId})`)
-  }
-
-  function onRemoveConfirm(listId: number) {
-    removeListMutation.mutate(listId)
+  function onRemoveConfirm() {
+    removeListMutation.mutate(list.id)
+    closeActionSheet()
   }
 
   return (
-    <ul className={clsx('space-y-1 text-base font-semibold', className)}>
-      {lists.map(list => (
-        <Swipeable
-          Component="li"
-          key={list.id}
-          className={clsx(
-            'rounded-md',
-            'focus-within:ring-2 focus-within:ring-gray-600'
-          )}
-          after={
-            allowRemoval ? (
-              <button
-                className="
+    <Swipeable
+      Component="li"
+      key={list.id}
+      className={clsx(
+        'rounded-md',
+        'focus-within:ring-2 focus-within:ring-gray-600'
+      )}
+      after={
+        allowRemoval ? (
+          <button
+            className="
                   h-full px-3 
                   flex items-center justify-between  
                   text-white bg-red-500
@@ -66,47 +74,61 @@ export const TaskLists: React.FC<Props> = ({
                   active:bg-red-600
                   focus:outline-none
                   rounded-r-md
-                  "
-                onClick={() => {
-                  onRemoveIntent(list.id)
-                }}
-              >
-                <TrashIcon className="w-5 h-5" />
-              </button>
-            ) : undefined
-          }
+                "
+            disabled={isRemoving}
+            onClick={onRemoveIntent}
+          >
+            <TrashIcon className="w-5 h-5" />
+          </button>
+        ) : undefined
+      }
+    >
+      <>
+        <ActionSheet name={`remove-task-list(${list.id})`}>
+          <ActionSheet.Action destructive onClick={onRemoveConfirm}>
+            Delete task
+          </ActionSheet.Action>
+        </ActionSheet>
+        <Link
+          to={`/lists/${list.id}`}
+          className={clsx(
+            'flex items-center justify-between',
+            'bg-gray-700 px-4 py-2',
+            'rounded-md',
+            'transition ease-in duration-150',
+            'active:text-gray-400',
+            'focus:bg-gray-500'
+          )}
+          onClick={() => closeMenu()}
         >
-          <>
-            <ActionSheet name={`remove-task-list(${list.id})`}>
-              <ActionSheet.Action
-                destructive
-                onClick={() => onRemoveConfirm(list.id)}
-              >
-                Delete task
-              </ActionSheet.Action>
-            </ActionSheet>
-            <Link
-              to={`/lists/${list.id}`}
-              className={clsx(
-                'flex items-center justify-between',
-                'bg-gray-700 px-4 py-2',
-                'rounded-md',
-                'transition ease-in duration-150',
-                'active:text-gray-400',
-                'focus:bg-gray-500'
-              )}
-              onClick={() => closeMenu()}
-            >
-              <span className="truncate">{list.title}</span>
-              {list.tasksCount !== '0' && (
-                <span className="ml-4 text-gray-400">
-                  {list.tasksCount}{' '}
-                  {pluralize(parseInt(list.tasksCount, 10), 'task', 'tasks')}
-                </span>
-              )}
-            </Link>
-          </>
-        </Swipeable>
+          <span className="truncate">{list.title}</span>
+          {isRemoving && <Spin className="w-5 h-5 text-opacity-30" />}
+          {!isRemoving && list.tasksCount !== '0' && (
+            <span className="ml-4 text-gray-400">
+              {list.tasksCount}{' '}
+              {pluralize(parseInt(list.tasksCount, 10), 'task', 'tasks')}
+            </span>
+          )}
+        </Link>
+      </>
+    </Swipeable>
+  )
+}
+
+export const TaskLists: React.FC<Props> = ({
+  allowRemoval = true,
+  className
+}) => {
+  const {data: lists} = useListsQuery()
+
+  if (isUndefined(lists)) {
+    return null
+  }
+
+  return (
+    <ul className={clsx('space-y-1 text-base font-semibold', className)}>
+      {lists.map(list => (
+        <TaskListItem key={list.id} list={list} allowRemoval={allowRemoval} />
       ))}
     </ul>
   )
