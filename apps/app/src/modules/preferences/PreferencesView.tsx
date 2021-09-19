05d8ae882195cc0ce1d21bcd0024775a19bf97fe
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-09-19 23:44:33 +0300

    feat: add theme preview

--- apps/app/src/modules/preferences/PreferencesView.tsx
+++ apps/app/src/modules/preferences/PreferencesView.tsx
@@ -3,6 +3,8 @@ import {MainView} from '@/views/MainView'
 import clsx from 'clsx'
 import noop from 'lodash/fp/noop'
 import {useStore} from 'effector-react'
+import DarkThemePreview from '@/assets/images/dark-theme.jpg'
+import LightThemePreview from '@/assets/images/light-theme.jpg'
 import {$isDarkMode, toggleDarkMode} from './darkmode'
 
 type TogglePropsT = {
@@ -62,6 +64,18 @@ export const PreferencesView: React.FC = () => {
       <div className="mt-2 px-4 w-full flex justify-between">
         <span className="font-semibold text-lg ">Dark mode</span>
         <Toggle isChecked={isDarkMode} onChange={toggleDarkMode} />
+      </div>
+      <div className="grid grid-cols-2 gap-4 p-4">
+        <img
+          className="w-full rounded-lg shadow-md border-2 border-gray-200 dark:border-dark-400"
+          src={DarkThemePreview}
+          alt="dark theme preview"
+        />
+        <img
+          className="w-full rounded-lg shadow-md border-2 border-gray-200 dark:border-dark-400"
+          src={LightThemePreview}
+          alt="light theme preview"
+        />
       </div>
     </MainView>
   )
