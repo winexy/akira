fa5516532648965e0f5e7dcd2f722e57f13f4aef
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-03 00:51:58 +0300

    refactor: remove dead code

--- apps/app/src/firebase/firebase.ts
+++ apps/app/src/firebase/firebase.ts
@@ -2,15 +2,9 @@ import firebase from 'firebase/app'
 import {config} from '@config/app'
 import 'firebase/analytics'
 import 'firebase/auth'
-import 'firebase/firestore'
 
 firebase.initializeApp(config.firebase)
 
 export {firebase}
-
 export const auth = firebase.auth()
 export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
-
-export const db = firebase.firestore()
-
-export const {arrayUnion, arrayRemove} = firebase.firestore.FieldValue
