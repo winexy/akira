80a60f2e10ec4d2b8fe76f2e4e1a4e9a669d047e
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-03 00:18:19 +0300

    refactor: move spin to shared ui

--- apps/app/src/components/ActionToast.tsx
+++ apps/app/src/components/ActionToast.tsx
@@ -1,7 +1,7 @@
 import clsx, {ClassValue} from 'clsx'
 import React, {useEffect, useRef, useState} from 'react'
 import {CSSTransition} from 'react-transition-group'
-import {Spin} from './Spin'
+import {Spin} from '@shared/ui/spin'
 import './ActionToast.css'
 
 type ActionToastButtonProps = NativeButtonProps & {
