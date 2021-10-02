bd92c966e047b54e4d1fbfd4e3f16cb7e04aad51
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-02 23:55:09 +0300

    refactor: move checkbox to shared ui layer

--- apps/app/src/shared/ui/checkbox/checkbox.test.jsx
+++ apps/app/src/shared/ui/checkbox/checkbox.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from '@testing-library/react'
-import {Checkbox} from './Checkbox'
+import {Checkbox} from '.'
 
 test('Checkbox', () => {
   render(<Checkbox />)
