719491a3ceaba487be952289c3ebdab5b862430a
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-02 22:44:14 +0300

    refactor: move TaskFrom to tasks module

--- src/modules/tasks/components/TaskForm.tsx
+++ src/modules/tasks/components/TaskForm.tsx
@@ -32,11 +32,14 @@ import {CalendarIcon} from '@heroicons/react/outline'
 import {CreateTaskPayload} from '@modules/tasks/types.d'
 import isTomorrow from 'date-fns/isTomorrow'
 import isToday from 'date-fns/isToday'
-import {Tag} from '../Tag/Tag'
-import {useListsQuery} from '../../modules/lists/hooks/index'
-import {BottomSheet} from '../BottomSheet/BottomSheet'
-import {showBottomSheet, hideBottomSheet} from '../../store/bottom-sheet/index'
-import {DatePicker, DatePickerSheet} from '../DatePicker'
+import {Tag} from '../../../components/Tag/Tag'
+import {useListsQuery} from '../../lists/hooks/index'
+import {BottomSheet} from '../../../components/BottomSheet/BottomSheet'
+import {
+  showBottomSheet,
+  hideBottomSheet
+} from '../../../store/bottom-sheet/index'
+import {DatePicker, DatePickerSheet} from '../../../components/DatePicker'
 
 type TaskFormProps = {
   onSubmit(payload: CreateTaskPayload): void
