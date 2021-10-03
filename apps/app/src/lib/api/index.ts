5ae7b1dfee288d60d8afc90f346359b24fd34a7f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-03 18:51:47 +0300

    refactor: remove trailing slash

--- apps/app/src/lib/api/index.ts
+++ apps/app/src/lib/api/index.ts
@@ -1,12 +1,13 @@
 import {auth} from '@/firebase/firebase'
 import axios, {AxiosError} from 'axios'
 import {config as appConfig} from '@config/app'
-import {identity} from 'lodash/fp'
+import identity from 'lodash/fp/identity'
+import replace from 'lodash/fp/replace'
+
+const removeTrailingSlash = (url: string) => replace(/\/$/, '', url)
 
 const api = axios.create({
-  baseURL: appConfig.api.url.endsWith('/')
-    ? appConfig.api.url
-    : `${appConfig.api.url}/`
+  baseURL: removeTrailingSlash(appConfig.api.url)
 })
 
 api.interceptors.request.use(async config => {
