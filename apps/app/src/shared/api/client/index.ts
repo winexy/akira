d9f6592de278fa9f3f81199b87946b22ea2667aa
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-04 23:51:09 +0300

    feat: inject api base url with interceptor

--- apps/app/src/shared/api/client/index.ts
+++ apps/app/src/shared/api/client/index.ts
@@ -1,15 +1,14 @@
 import axios from 'axios'
 import identity from 'lodash/fp/identity'
-import replace from 'lodash/fp/replace'
-import {config as appConfig} from 'shared/config'
-import {extractError, mutableInjectToken} from './interceptors'
+import {
+  extractError,
+  mutableInjectToken,
+  mutableInjectBaseURL
+} from './interceptors'
 
-const removeTrailingSlash = (url: string) => replace(/\/$/, '', url)
+const api = axios.create()
 
-const api = axios.create({
-  baseURL: removeTrailingSlash(appConfig.api.url)
-})
-
+api.interceptors.request.use(mutableInjectBaseURL)
 api.interceptors.request.use(mutableInjectToken)
 api.interceptors.response.use(identity, extractError)
 
