460543abfb8773f5f33e1978e48e673750b6ab80
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-04 23:52:09 +0300

    feat: control api version

--- apps/app/src/features/api-version/ui/index.tsx
+++ apps/app/src/features/api-version/ui/index.tsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react'
-import {config} from 'shared/config'
+import React from 'react'
 import {SegmentedControl, Segment} from 'shared/ui/segmented-control'
+import {useStore} from 'effector-react'
+import {apiModel} from 'shared/api'
 
 export const ApiVersion: React.FC = () => {
-  const [activeId, setActiveId] = useState('dev')
-
-  if (config.env.prod) {
-    return null
-  }
+  const apiVersion = useStore(apiModel.$apiVersion)
 
   return (
     <div className="mt-2 px-4 w-full">
       <span className="font-semibold text-lg">API Version</span>
-      <SegmentedControl activeId={activeId} onChange={setActiveId}>
-        <Segment id="prod">Production</Segment>
-        <Segment id="dev">Development</Segment>
+      <SegmentedControl
+        activeId={apiVersion}
+        onChange={apiModel.changeApiVersion}
+      >
+        <Segment id={apiModel.ApiVersionEnum.Prod}>Production</Segment>
+        <Segment id={apiModel.ApiVersionEnum.Dev}>Development</Segment>
       </SegmentedControl>
     </div>
   )
