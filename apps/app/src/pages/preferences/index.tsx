7e1e4be3972e8683a109f1d3b9878f2f55a0c25e
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-04 23:33:44 +0300

    feat: add api version ui

--- apps/app/src/pages/preferences/index.tsx
+++ apps/app/src/pages/preferences/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {PageView} from 'shared/ui/page-view'
 import {DarkMode} from 'features/darkmode/ui'
+import {ApiVersion} from 'features/api-version/ui'
 
 const PreferencesPage: React.FC = () => {
   return (
     <PageView>
       <DarkMode />
+      <ApiVersion />
     </PageView>
   )
 }
