0f680aad490d2ac9bfa901526286d862788cc1f5
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-07 00:02:56 +0300

    feat: add active state for buttons

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -14,14 +14,14 @@ const xs = 'px-2 py-1'
 const sm = 'px-3 py-2'
 const md = 'px-6 py-3'
 const blue =
-  'bg-blue-500 border border-blue-600 active:bg-blue-600 active:border-blue-700 text-white shadow-md active:shadow'
+  'bg-blue-500 border border-blue-600 active:bg-blue-600 active:border-blue-700 text-white shadow-md active:shadow-inner'
 const red =
-  'bg-red-500 border border-red-600 active:bg-red-600 active:border-red-700 text-white shadow-md active:shadow focus:ring-red-300'
+  'bg-red-500 border border-red-600 active:bg-red-600 active:border-red-700 text-white shadow-md active:shadow-inner focus:ring-red-300'
 const outline =
   'bg-white dark:bg-dark-600 border border-gray-200 dark:border-gray-600 text-gray-700 dark:text-white active:bg-gray-100 dark:active:bg-dark-400 active:border-gray-200 active:shadow-inner focus:ring'
-const transparent = `bg-white bg-opacity-10 border border-white border-opacity-20 active:bg-white active:bg-opacity-20 text-white`
+const transparent = `bg-white bg-opacity-10 border border-white border-opacity-20 active:shadow-inner active:bg-white active:bg-opacity-20 text-white`
 const indigo =
-  'text-white bg-indigo-500 border border-indigo-400 active:bg-indigo-400 active:border-indigo-300 focuse:ring-indigo-300'
+  'text-white bg-indigo-500 border border-indigo-400 active:shadow-inner active:bg-indigo-400 active:border-indigo-300 focuse:ring-indigo-300'
 
 type ButtonSize = 'xs' | 'sm' | 'md'
 type ButtonVariant = 'blue' | 'red' | 'outline' | 'transparent' | 'indigo'
