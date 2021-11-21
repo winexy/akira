9cd3cf14958201832d8af968870347a7bb7fef97
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-11-21 15:59:24 +0300

    feat: improve checklist ui

--- src/features/checklist/ui/Checklist.tsx
+++ src/features/checklist/ui/Checklist.tsx
@@ -5,7 +5,6 @@ import {XIcon} from '@heroicons/react/solid'
 
 import {Checkbox} from 'shared/ui/checkbox'
 import {TaskId, Todo} from 'modules/tasks/types.d'
-import {List} from 'shared/ui/list'
 import {useRemoveTodoMutation, usePatchTodoMutation} from '../model'
 
 type Props = {
@@ -22,41 +21,45 @@ export const Checklist: React.FC<Props> = ({taskId, checklist}) => {
   }
 
   return (
-    <List className="mt-2">
+    <ul className="mt-2 px-4 space-y-1">
       {checklist.map(todo => (
-        <List.Item
+        <li
           key={todo.id}
-          className={clsx('px-4 py-1', {'line-through': todo.is_completed})}
+          className={clsx('rounded-md flex', {
+            'line-through': todo.is_completed
+          })}
         >
-          <Checkbox
-            isChecked={todo.is_completed}
-            className="mr-4"
-            size="sm"
-            onChange={() =>
-              patchTodoMutation.mutate({
-                todoId: todo.id,
-                patch: {
-                  is_completed: !todo.is_completed
-                }
-              })
-            }
-          />
-          {todo.title}
+          <div className="py-1 flex items-center">
+            <Checkbox
+              isChecked={todo.is_completed}
+              className="mr-3"
+              size="sm"
+              onChange={() =>
+                patchTodoMutation.mutate({
+                  todoId: todo.id,
+                  patch: {
+                    is_completed: !todo.is_completed
+                  }
+                })
+              }
+            />
+            {todo.title}
+          </div>
           <button
             className={clsx(
               'ml-auto -mr-3 w-10 h-10',
               'flex items-center justify-center',
               'text-red-500 rounded',
               'transition ease-in duration-150',
-              'active:text-red-600 active:bg-gray-300',
+              'active:text-red-600 active:bg-gray-100 dark:active:bg-dark-400',
               'focus:outline-none focus:bg-gray-200 focus:bg-opacity-75'
             )}
             onClick={() => removeTodoMutation.mutate(todo.id)}
           >
             <XIcon className="w-4 h-4" />
           </button>
-        </List.Item>
+        </li>
       ))}
-    </List>
+    </ul>
   )
 }
--- src/features/checklist/ui/ChecklistManager.tsx
+++ src/features/checklist/ui/ChecklistManager.tsx
@@ -1,8 +1,6 @@
 import isEmpty from 'lodash/fp/isEmpty'
-import React, {FormEventHandler, useLayoutEffect, useRef, useState} from 'react'
+import React, {FormEventHandler, useRef, useState} from 'react'
 import {ApiTask} from 'modules/tasks/types.d'
-import {ClipboardCheckIcon} from '@heroicons/react/solid'
-import {Button} from 'shared/ui/button'
 import clsx from 'clsx'
 import {Checklist} from './Checklist'
 import {useAddTodoMutation} from '../model'
@@ -13,15 +11,8 @@ type Props = {
 
 export const ChecklistManager: React.FC<Props> = ({task}) => {
   const [todoTitle, setTodoTitle] = useState('')
-  const [isTodoInputVisible, setIsTodoInputVisible] = useState(false)
   const todoInputRef = useRef<HTMLInputElement | null>(null)
   const addTodoMutation = useAddTodoMutation(task.id)
-
-  useLayoutEffect(() => {
-    if (isTodoInputVisible) {
-      todoInputRef.current?.focus()
-    }
-  }, [isTodoInputVisible])
 
   const onSubmit: FormEventHandler = event => {
     event.preventDefault()
@@ -34,35 +25,25 @@ export const ChecklistManager: React.FC<Props> = ({task}) => {
 
   return (
     <>
-      <section className="mt-4 px-4">
-        {isEmpty(task.checklist) && !isTodoInputVisible ? (
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={() => setIsTodoInputVisible(true)}
-          >
-            <ClipboardCheckIcon className="mr-2 w-4 h-4" />
-            Add Checklist
-          </Button>
-        ) : (
-          <form onSubmit={onSubmit}>
-            <input
-              ref={todoInputRef}
-              value={todoTitle}
-              placeholder="Add todo..."
-              className={clsx(
-                'bg-gray-200 dark:bg-dark-400 bg-opacity-50 px-4 py-2 rounded-md border shadow-inner',
-                'appearance-none border border-gray-300 dark:border-dark-200',
-                'focus:outline-none focus:border-indigo-500 dark:focus:border-indigo-500'
-              )}
-              onInput={e => setTodoTitle((e.target as HTMLInputElement).value)}
-              onBlur={() => setIsTodoInputVisible(false)}
-              enterKeyHint="send"
-            />
-          </form>
-        )}
-      </section>
+      <h2 className="mt-2 px-4 font-bold text-2xl">Checklist</h2>
       <Checklist taskId={task.id} checklist={task.checklist} />
+      <section className="px-4">
+        <form onSubmit={onSubmit}>
+          <input
+            ref={todoInputRef}
+            value={todoTitle}
+            placeholder="Add item..."
+            className={clsx(
+              'w-full bg-transparent py-2 rounded-md border',
+              'appearance-none border-none',
+              'focus:outline-none focus:animate-none',
+              {'animate-pulse': !isEmpty(todoTitle)}
+            )}
+            onInput={e => setTodoTitle((e.target as HTMLInputElement).value)}
+            enterKeyHint="send"
+          />
+        </form>
+      </section>
     </>
   )
 }
