39d8f4329a1579ef4c6919aff4ff9c9c58a01ef9
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-11-21 23:16:08 +0300

    feat: add akira.app sign

--- apps/app/src/pages/auth/index.tsx
+++ apps/app/src/pages/auth/index.tsx
@@ -1,5 +1,5 @@
+import clsx from 'clsx'
 import React from 'react'
-import {LoginIcon} from '@heroicons/react/solid'
 import {useFirebaseAuth} from 'shared/lib/firebase'
 import {Akira} from 'shared/ui/akira'
 import {Button} from 'shared/ui/button'
@@ -10,6 +10,20 @@ export const AuthPage: React.FC = () => {
 
   return (
     <div className="bg-white dark:bg-dark-700 flex-1 flex-col flex justify-center items-center p-6">
+      <h1
+        className={clsx(
+          'absolute top-0 mt-3',
+          'text-sm font-bold text-center font-mono',
+          'bg-gradient-to-r from-indigo-500 to-pink-600'
+        )}
+        style={{
+          // @ts-expect-error
+          '-webkit-background-clip': 'text',
+          '-webkit-text-fill-color': 'transparent'
+        }}
+      >
+        akira.app
+      </h1>
       <div className="mt-auto">
         <Akira className="w-36 h-36" />
       </div>
