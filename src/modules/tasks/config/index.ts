291abcfc2d39254f0dba426f99ea7447096406e7
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-01-03 17:41:58 +0600

    refactor: improve task list query key

--- src/modules/tasks/config/index.ts
+++ src/modules/tasks/config/index.ts
@@ -1,8 +1,9 @@
 import constant from 'lodash/constant'
 
 export const TaskQuery = {
-  One: (taskId: string) => ['task', taskId],
-  All: constant(['tasks']),
-  MyDay: constant(['myday']),
-  Week: constant(['week'])
+  One: (taskId: string) => ['tasks', taskId],
+  All: constant(['tasks', 'all']),
+  MyDay: constant(['tasks', 'myday']),
+  Week: constant(['tasks', 'week']),
+  List: (taskId: string) => ['tasks', 'lists', taskId]
 }
