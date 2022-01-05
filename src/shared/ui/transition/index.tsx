6e49a5e9dbfc9327ba01e61677be964e7835a5c4
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-01-05 13:46:45 +0600

    feat: add scale transition

--- src/shared/ui/transition/index.tsx
+++ src/shared/ui/transition/index.tsx
@@ -1,2 +1,9 @@
-export {FadeTransition} from './fade/FadeTransition'
-export {ShiftTransition} from './shift/ShiftTransition'
+import {FadeTransition} from './fade/FadeTransition'
+import {ShiftTransition} from './shift/ShiftTransition'
+import {ScaleTransition} from './scale/ScaleTransition'
+
+export const Transition = {
+  Scale: ScaleTransition,
+  Shift: ShiftTransition,
+  Fade: FadeTransition
+}
