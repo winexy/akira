2096f66a7787748cfea2092773e9115f7bf9c8a7
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-01-05 13:46:45 +0600

    feat: add scale transition

---
+++ apps/app/src/shared/ui/transition/scale/ScaleTransition.tsx
@@ -0,0 +1,44 @@
+import React, {FC} from 'react'
+import {CSSTransition} from 'react-transition-group'
+import {CSSTransitionProps} from 'react-transition-group/CSSTransition'
+import './ui-scale.css'
+
+type Props = Omit<CSSTransitionProps, 'timeout'> & {
+  timeout?: number
+  scaleFrom?: number
+  scaleTo?: number
+}
+
+export const ScaleTransition: FC<Props> = ({
+  appear = false,
+  in: inProp = true,
+  timeout = 300,
+  children,
+  scaleFrom = 0.8,
+  scaleTo = 1,
+  ...props
+}) => {
+  const style = {
+    '--ui-scale-enter-duration': `${timeout}ms`,
+    '--ui-scale-exit-duration': `${timeout}ms`,
+    '--ui-scale-from': scaleFrom,
+    '--ui-scale-to': scaleTo
+  }
+
+  const child = React.Children.only(children)
+  const element = React.isValidElement(child)
+    ? React.cloneElement(child, {style})
+    : null
+
+  return (
+    <CSSTransition
+      appear={appear}
+      in={inProp}
+      timeout={timeout}
+      classNames="ui-scale"
+      {...props}
+    >
+      {element}
+    </CSSTransition>
+  )
+}
