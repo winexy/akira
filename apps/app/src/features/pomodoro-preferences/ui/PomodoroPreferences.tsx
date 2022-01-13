4e9152c7090c106b09da992d08daaa3ce32b3992
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-01-13 17:35:08 +0600

    feat: add basic preferences layout

--- apps/app/src/features/pomodoro-preferences/ui/PomodoroPreferences.tsx
+++ apps/app/src/features/pomodoro-preferences/ui/PomodoroPreferences.tsx
@@ -1,21 +1,35 @@
 import React, {FC} from 'react'
 import {UniversalDrawer} from 'widgets/universal-drawer'
 
+const Input = () => (
+  <input
+    type="number"
+    min="0"
+    className="ml-6 appearance-none px-3 py-1 rounded-md border border-gray-100"
+  />
+)
+
+const Label: FC = ({children}) => (
+  <label className="flex justify-between items-center font-semibold">
+    {children}
+  </label>
+)
+
 export const PomodoroPreferences: FC = () => {
   return (
-    <UniversalDrawer name="pomodoro-settings" className="p-6 space-y-2">
-      <label className="flex justify-between font-semibold">
+    <UniversalDrawer name="pomodoro-settings" className="p-6 space-y-4">
+      <Label>
         Focus time duration
-        <input type="number" className="ml-6" />
-      </label>
-      <label className="flex justify-between font-semibold">
+        <Input />
+      </Label>
+      <Label>
         Short break duration
-        <input type="number" className="ml-6" />
-      </label>
-      <label className="flex justify-between font-semibold">
+        <Input />
+      </Label>
+      <Label>
         Long break duration
-        <input type="number" className="ml-6" />
-      </label>
+        <Input />
+      </Label>
     </UniversalDrawer>
   )
 }
