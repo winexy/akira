8cbb0a03b38e43720283aa3c842ffc7645399814
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-01-13 20:33:06 +0600

    feat: add settings draft

--- apps/app/src/features/pomodoro-preferences/ui/PomodoroPreferences.tsx
+++ apps/app/src/features/pomodoro-preferences/ui/PomodoroPreferences.tsx
@@ -1,8 +1,10 @@
-import React, {FC} from 'react'
+import React, {FC, FormEventHandler, useState} from 'react'
 import {useStore} from 'effector-react'
 import clsx from 'clsx'
 import {UniversalDrawer, universalDrawerModel} from 'widgets/universal-drawer'
 import {pomodoroModel} from 'entities/pomodoro'
+import {saveSettings} from 'entities/pomodoro/model'
+import {Button} from 'shared/ui/button'
 
 type InputProps = {} & React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
@@ -33,27 +35,38 @@ export const PomodoroPreferences: FC = () => {
   const shortBreakDuration = useStore(pomodoroModel.$shortBreakDuration)
   const longBreakDuration = useStore(pomodoroModel.$longBreakDuration)
 
+  const [focusDurationDraft, setFocusDuration] = useState(focusDuration)
+  const [shortBreakDurationDraft, setShortBreakDuration] = useState(
+    shortBreakDuration
+  )
+  const [longBreakDurationDraft, setLongBreakDuration] = useState(
+    longBreakDuration
+  )
+
+  const onSubmit: FormEventHandler = event => {
+    event.preventDefault()
+    universalDrawerModel.hideDrawer()
+
+    pomodoroModel.saveSettings({
+      focusDuration: focusDurationDraft,
+      shortBreakDuration: shortBreakDurationDraft,
+      longBreakDuration: longBreakDurationDraft
+    })
+  }
+
   return (
     <UniversalDrawer name="pomodoro-settings" className="p-6">
-      <form
-        className="h-full flex flex-col"
-        onSubmit={event => {
-          event.preventDefault()
-          universalDrawerModel.hideDrawer()
-        }}
-      >
+      <form className="h-full flex flex-col" onSubmit={onSubmit}>
         <div className="space-y-4 mb-6 sm:w-80">
           <Label>
             Focus time (min)
             <Input
               type="number"
               min="0"
-              value={focusDuration}
+              value={focusDurationDraft}
               className="ml-6 w-20"
               onChange={event =>
-                pomodoroModel.changeFocusDuration(
-                  parseInt(event.target.value, 10)
-                )
+                setFocusDuration(parseInt(event.target.value, 10))
               }
             />
           </Label>
@@ -62,12 +75,10 @@ export const PomodoroPreferences: FC = () => {
             <Input
               type="number"
               min="0"
-              value={shortBreakDuration}
+              value={shortBreakDurationDraft}
               className="ml-6 w-20"
               onChange={event =>
-                pomodoroModel.changeShortBreakDuration(
-                  parseInt(event.target.value, 10)
-                )
+                setShortBreakDuration(parseInt(event.target.value, 10))
               }
             />
           </Label>
@@ -76,16 +87,17 @@ export const PomodoroPreferences: FC = () => {
             <Input
               type="number"
               min="0"
-              value={longBreakDuration}
+              value={longBreakDurationDraft}
               className="ml-6 w-20"
               onChange={event =>
-                pomodoroModel.changeLongBreakDuration(
-                  parseInt(event.target.value, 10)
-                )
+                setLongBreakDuration(parseInt(event.target.value, 10))
               }
             />
           </Label>
         </div>
+        <Button type="submit" size="md" className="mt-auto">
+          Apply
+        </Button>
       </form>
     </UniversalDrawer>
   )
