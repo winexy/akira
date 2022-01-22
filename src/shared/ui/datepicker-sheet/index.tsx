5a26a51b048f6e94cc4df0c2fc3b507efb57ff1f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-01-22 16:58:36 +0600

    feat: close datepicker on shortcut select

--- src/shared/ui/datepicker-sheet/index.tsx
+++ src/shared/ui/datepicker-sheet/index.tsx
@@ -2,13 +2,18 @@ import React, {ReactNode} from 'react'
 import format from 'date-fns/format'
 import {Button} from 'shared/ui/button'
 import {UniversalDrawer, universalDrawerModel} from 'widgets/universal-drawer'
+import isFunction from 'lodash/isFunction'
+
+type FixedChildrenProps = {
+  hide(): void
+}
 
 type DatePickerSheetProps = {
   name?: string
   title: React.ReactNode
   date: Date | null
   onApply?(): void
-  fixedChildren?: ReactNode
+  fixedChildren?: ReactNode | ((props: FixedChildrenProps) => ReactNode)
 }
 
 export const DatePickerSheet: React.FC<DatePickerSheetProps> = ({
@@ -19,6 +24,7 @@ export const DatePickerSheet: React.FC<DatePickerSheetProps> = ({
   fixedChildren,
   onApply
 }) => {
+  const hide = () => universalDrawerModel.hideDrawer()
   return (
     <UniversalDrawer
       name={name}
@@ -27,14 +33,14 @@ export const DatePickerSheet: React.FC<DatePickerSheetProps> = ({
       <h2 className="mt-4 px-4 text-center font-bold text-2xl">{title}</h2>
       <div className="px-4">{children}</div>
       <div className="mt-auto sticky bottom-0 px-4">
-        {fixedChildren}
+        {isFunction(fixedChildren) ? fixedChildren({hide}) : fixedChildren}
         <Button
           size="md"
           type="button"
           className="mt-4 w-full"
           onClick={event => {
             event.stopPropagation()
-            universalDrawerModel.hideDrawer()
+            hide()
             onApply?.()
           }}
         >
