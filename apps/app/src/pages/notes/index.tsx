735c6c69734f141467c059f9e114104913f58454
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-02-12 22:41:52 +0300

    refactor: add dark theme styles

--- apps/app/src/pages/notes/index.tsx
+++ apps/app/src/pages/notes/index.tsx
@@ -10,10 +10,11 @@ import {DocumentTextIcon} from '@heroicons/react/outline'
 import {useHistory} from 'react-router'
 import clsx from 'clsx'
 import NotePage from 'pages/note'
+import isNull from 'lodash/isNull'
 
 const Notes: FC = () => {
   const history = useHistory()
-  const [noteUUID, setNoteUUID] = useState<string>(null)
+  const [noteUUID, setNoteUUID] = useState<string | null>(null)
 
   const {data: notes} = useQuery('notes', () =>
     api
@@ -31,16 +32,23 @@ const Notes: FC = () => {
 
   return (
     <PageView className="flex-1 grid grid-cols-12">
-      <div className="col-span-3 h-full px-2 border-r border-t">
+      <div className="col-span-3 h-full px-2 border-r border-t dark:border-dark-500">
         <div className="mt-4 px-2 flex justify-between">
-          <h1 className="font-bold text-4xl">All notes</h1>
-          <Button
+          <h1 className="font-bold text-3xl">All notes</h1>
+          <button
             disabled={createEmptyNoteMutation.isLoading}
-            variant="outline"
+            className={clsx(
+              'w-10 h-10 border rounded-lg dark:border-dark-400',
+              'flex items-center justify-center',
+              'transition',
+              'hover:border-gray-300 dark:hover:border-dark-300',
+              'active:bg-gray-100 dark:active:bg-dark-500',
+              'focus:outline-none focus:border-blue-500 dark:focus:border-blue-500'
+            )}
             onClick={createEmptyNote}
           >
             <PlusIcon width={32} height={32} />
-          </Button>
+          </button>
         </div>
         {isEmpty(notes) && <Empty message="You haven't created any note" />}
         {Array.isArray(notes) && (
@@ -50,11 +58,11 @@ const Notes: FC = () => {
                 <button
                   className={clsx(
                     'w-full flex px-2 py-2',
-                    'rounded-md',
-                    'hover:bg-gray-100',
-                    'transition',
-                    'hover:text-blue-400 active:text-indigo-500',
-                    'focus:outline-none focus:text-blue-500'
+                    'rounded-md dark:text-white',
+                    'hover:bg-gray-100 hover:text-blue-500 dark:hover:bg-dark-500',
+                    'transition ease-in duration-150',
+                    'active:bg-gray-200 active:text-indigo-600',
+                    'focus:outline-none focus:bg-gray-100 focus:text-blue-500 dark:focus:bg-dark-400'
                   )}
                   onClick={() => setNoteUUID(note.uuid)}
                 >
@@ -67,11 +75,11 @@ const Notes: FC = () => {
           </ul>
         )}
       </div>
-      <NotePage
-        key={noteUUID}
-        uuid={noteUUID}
-        className="border-t col-span-9 flex-1"
-      />
+      <div className="border-t col-span-9 flex dark:border-dark-500">
+        {!isNull(noteUUID) && (
+          <NotePage key={noteUUID} uuid={noteUUID} className="flex-1" />
+        )}
+      </div>
     </PageView>
   )
 }
