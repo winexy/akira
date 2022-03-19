41274794b7620d1bdc746a34f833f4ea7cf2a04f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-03-19 15:26:27 +0300

    refactor: remove legacy tasks config

--- apps/app/src/modules/tasks/config/index.ts
+++
@@ -1,9 +0,0 @@
-import constant from 'lodash/constant'
-
-export const TaskQuery = {
-  One: (taskId: string) => ['tasks', taskId],
-  All: constant(['tasks', 'all']),
-  MyDay: constant(['tasks', 'myday']),
-  Week: constant(['tasks', 'week']),
-  List: (taskId: string) => ['tasks', 'lists', taskId],
-}
