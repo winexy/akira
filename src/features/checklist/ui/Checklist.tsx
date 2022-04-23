b439bcd1f55bc5641119031123de84cdd2d055b8
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-04-23 22:25:25 +0600

    feat: improve checklist item edit ui

--- src/features/checklist/ui/Checklist.tsx
+++ src/features/checklist/ui/Checklist.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import clsx from 'clsx'
 import isEmpty from 'lodash/fp/isEmpty'
-import {XIcon} from '@heroicons/react/solid'
-
+import {XIcon, CheckIcon} from '@heroicons/react/solid'
 import {Checkbox} from 'shared/ui/checkbox'
+import {Spin} from 'shared/ui/spin'
 import {TaskId, Todo} from 'modules/tasks/types.d'
 import {TextArea} from 'modules/tasks/components'
 import {useRemoveTodoMutation, usePatchTodoMutation} from '../model'
@@ -21,6 +21,11 @@ type TodoProps = {
 const TodoItem: React.FC<TodoProps> = ({taskId, todo}) => {
   const patchTodoMutation = usePatchTodoMutation(taskId)
   const removeTodoMutation = useRemoveTodoMutation(taskId)
+  const [isFocused, setIsFocused] = useState(false)
+
+  const isUpdateInProgress = patchTodoMutation.isLoading
+  const isEditInProgress = !isUpdateInProgress && isFocused
+  const isIdle = !isUpdateInProgress && !isFocused
 
   return (
     <li
@@ -48,30 +53,57 @@ const TodoItem: React.FC<TodoProps> = ({taskId, todo}) => {
           }
         />
       </label>
-      <TextArea
-        value={todo.title}
-        onChange={newTitle => {
-          patchTodoMutation.mutate({
-            todoId: todo.id,
-            patch: {
-              title: newTitle,
-            },
-          })
-        }}
-      />
-      <button
-        className={clsx(
-          'ml-auto -mr-3 w-10 h-10',
-          'flex items-center justify-center',
-          'text-red-500 rounded',
-          'transition ease-in duration-150',
-          'active:text-red-600 active:bg-gray-100 dark:active:bg-dark-400',
-          'focus:outline-none focus:bg-gray-200 focus:bg-opacity-75',
+      <form onSubmit={e => e.preventDefault()}>
+        <TextArea
+          value={todo.title}
+          onChange={newTitle => {
+            patchTodoMutation.mutate({
+              todoId: todo.id,
+              patch: {
+                title: newTitle,
+              },
+            })
+          }}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </form>
+      <div className="-mr-3 ml-auto h-10 flex items-center">
+        {isUpdateInProgress && (
+          <div className="w-10 flex justify-center items-center">
+            <Spin className="w-4 h-4 text-gray-300" />
+          </div>
         )}
-        onClick={() => removeTodoMutation.mutate(todo.id)}
-      >
-        <XIcon className="w-4 h-4" />
-      </button>
+        {isEditInProgress && (
+          <button
+            className={clsx(
+              'w-10',
+              'flex items-center justify-center',
+              'text-green-500 rounded',
+              'transition ease-in duration-150',
+              'active:text-green-600 active:bg-gray-100 dark:active:bg-dark-400',
+              'focus:outline-none focus:bg-gray-200 focus:bg-opacity-75',
+            )}
+          >
+            <CheckIcon className="w-4 h-4" />
+          </button>
+        )}
+        {isIdle && (
+          <button
+            className={clsx(
+              'w-10',
+              'flex items-center justify-center',
+              'text-red-500 rounded',
+              'transition ease-in duration-150',
+              'active:text-red-600 active:bg-gray-100 dark:active:bg-dark-400',
+              'focus:outline-none focus:bg-gray-200 focus:bg-opacity-75',
+            )}
+            onClick={() => removeTodoMutation.mutate(todo.id)}
+          >
+            <XIcon className="w-4 h-4" />
+          </button>
+        )}
+      </div>
     </li>
   )
 }
