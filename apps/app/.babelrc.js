71db59b67074968f37dba8acba916363f015773a
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-05-15 12:57:48 +0600

    chore: configure babel preset-env for using async/await in testing

--- apps/app/.babelrc.js
+++ apps/app/.babelrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   presets: [
     '@babel/preset-react',
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
     '@babel/preset-typescript',
   ],
 }
