15bfc8ff364d2ceeb0a0aadb6fc22ec51e5c5af3
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-05-15 23:51:21 +0600

    feat: move menu burger to left

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import React from 'react'
 import {useFirebaseAuth} from 'shared/lib/firebase'
 import {Akira} from 'shared/ui/akira'
+import {AkiraTitle} from 'shared/ui/akira-title'
 import {Button} from 'shared/ui/button'
 import GoogleIcon from 'src/assets/icons/google.svg'
 
@@ -10,19 +11,7 @@ export const AuthPage: React.FC = () => {
 
   return (
     <div className="bg-white dark:bg-dark-700 flex-1 flex-col flex justify-center items-center p-6">
-      <h1
-        className={clsx(
-          'absolute top-0 mt-3',
-          'text-sm font-bold text-center font-mono',
-          'bg-gradient-to-r from-indigo-500 to-pink-600',
-        )}
-        style={{
-          WebkitBackgroundClip: 'text',
-          WebkitTextFillColor: 'transparent',
-        }}
-      >
-        akira.app
-      </h1>
+      <AkiraTitle className="absolute top-0 mt-3" size="lg" />
       <div className="mt-auto">
         <Akira className="w-36 h-36" />
       </div>
