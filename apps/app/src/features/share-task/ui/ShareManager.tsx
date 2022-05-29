805e9066b28d15a458e9d4f829a7ac343d16da62
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-05-29 23:58:54 +0600

    feat: add user finding logic

--- apps/app/src/features/share-task/ui/ShareManager.tsx
+++ apps/app/src/features/share-task/ui/ShareManager.tsx
@@ -1,12 +1,26 @@
+import {useQuery} from 'react-query'
+import React, {useState, FC} from 'react'
 import map from 'lodash/fp/map'
-import React, {FC} from 'react'
 import {Button} from 'shared/ui/button'
 import {List} from 'shared/ui/list'
 import {WIP} from 'shared/ui/tag'
 import {UniversalDrawer} from 'widgets/universal-drawer'
-import {shareTaskModel} from '../model/index'
+import {userApi} from 'entities/user'
+import {shareTaskModel} from '../model'
+
+const emailRegex = /\S+@\S+\.\S+/
+const isEmail = (email: string) => emailRegex.test(email)
 
 export const ShareManager: FC = () => {
+  const [email, setEmail] = useState('')
+  const findUserQuery = useQuery(
+    ['findUser', {email}],
+    () => userApi.findByEmail(email),
+    {
+      enabled: isEmail(email),
+    },
+  )
+
   return (
     <UniversalDrawer
       name={shareTaskModel.SHARE_MANAGER_KEY}
@@ -22,22 +36,31 @@ export const ShareManager: FC = () => {
       <div className="mt-2 flex">
         <input
           placeholder="teammate@example.com"
+          value={email}
           className="
-            flex-1 mr-4 px-3
+            flex-1 px-3 py-2
             shadow-inner rounded-md
             border
             focus:outline-none focus:outline-2
           "
+          onChange={event => setEmail(event.target.value)}
         />
-        <Button size="xs" className="ml-auto">
-          Send invite
-        </Button>
       </div>
+      {findUserQuery.data && (
+        <div className="mt-4 flex items-center">
+          <span className="mr-4 flex-1 truncate">
+            {findUserQuery.data.email}
+          </span>
+          <Button size="xs" className="ml-auto">
+            Send invite
+          </Button>
+        </div>
+      )}
       <h4 className="mt-4 font-bold text-lg">Members</h4>
       <List className="mt-2">
         {map(
           member => (
-            <List.Item>{member}</List.Item>
+            <List.Item key={member}>{member}</List.Item>
           ),
           ['ellie', 'ed', 'adele'],
         )}
