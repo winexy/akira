3315c47550274d92cdfa4dcc7ff30fdf6c87b691
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-10-23 21:36:11 +0600

    chore: move husky/commitlint/lint-staged to root

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  '*': 'echo TODO',
+  // '*.{js,jsx,ts,tsx}': 'eslint --fix',
+  // '*.{js,jsx,ts,tsx,json,css,md,yml}': 'prettier --write',
+  // './.github/actions/*/index.js': files => {
+  //   return files
+  //     .map(path => {
+  //       const [, action] = path.match(/actions\/(.+)\/index.js/)
+  //       return `yarn run compile-action --action=${action}`
+  //     })
+  //     .concat(`git add ./.github/actions/*/dist/index.js`)
+  // },
+}
